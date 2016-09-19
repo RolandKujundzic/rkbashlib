b8c188cddcd00a3730cad8f00d7c41592d347260
Roland Kujundzic <roland@inkoeln.com>
2016-09-19 10:58:13 +0200

    use _mkdir

--- src/git_update.sh
+++ src/git_update.sh
@@ -4,7 +4,7 @@
 # Update git components.
 #------------------------------------------------------------------------------
 function _git_update {
-	test -d php || mkdir php
+	_mkdir php
 	cd php
 	_git_checkout "https://github.com/RolandKujundzic" "rkphplib"
 	_git_checkout "rk@s1.dyn4.com:/data/git" "phplib"
