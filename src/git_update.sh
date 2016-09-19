04c4a239e591d37d7973586fd14b1276df1476e7
Roland Kujundzic <roland@inkoeln.com>
2016-09-19 10:51:26 +0200

    refactoring

--- src/git_update.sh
+++ src/git_update.sh
@@ -6,10 +6,8 @@
 function _git_update {
 	test -d php || mkdir php
 	cd php
-
 	_git_checkout "https://github.com/RolandKujundzic" "rkphplib"
 	_git_checkout "rk@s1.dyn4.com:/data/git" "phplib"
-
 	cd ..
 }
 
