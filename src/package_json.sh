9eb6c4951c210ea718c2c98a1422a391d7a760f2
Roland Kujundzic <roland@inkoeln.com>
2016-09-30 15:34:52 +0200

    initial

---
+++ src/package_json.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------------
+# Install or update npm packages. Create package.json and README.md if missing.
+#
+# @export NPM_PACKAGE="pkg1 .. pkgN"
+# @export NPM_PACKAGE_DEV="pkg1 ... pkgN"
+# @require _npm_module
+#-------------------------------------------------------------------------------
+function _package_json {
+
+	if ! test -f package.json; then
+		echo "create: package.json"
+		echo '{ "name": "ToDo", "version": "0.1.0", "title": "ToDo", "description": "ToDo", "repository": {} }' > package.json
+	fi
+
+	if ! test -f README.md; then
+		echo "create: README.md - adjust content"
+		echo "ToDo" > README.md
+	fi
+
+	local RUN_INSTALL=
+	local RUN_INSTALL_DEV=
+	local HAS_PKG=
+
+	for a in $NPM_PACKAGE; do
+		HAS_PKG=`grep $a package.json`
+		if ! test -z "$HAS_PKG"; then
+			RUN_INSTALL=1
+		fi
+	done
+
+	for a in $NPM_PACKAGE_DEV; do
+		HAS_PKG=`grep $a package.json`
+		if ! test -z "$HAS_PKG"; then
+			RUN_INSTALL_DEV=1
+		fi
+	done
+
+	if ! test -z "$RUN_INSTALL"; then
+		echo "run: npm install"
+		npm install
+	fi
+
+	if ! test -z "$RUN_INSTALL_DEV"; then
+		echo "run: npm install --dev"
+		npm install --dev
+	fi
+
+	for a in $NPM_PACKAGE; do
+		_npm_module $a --save
+	done
+
+	for a in $NPM_PACKAGE_DEV; do
+		_npm_module $a --save-dev
+	done
+}
+
