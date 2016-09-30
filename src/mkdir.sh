a9868748e4326f48030a6d7e6418e178367057af
Roland Kujundzic <roland@inkoeln.com>
2016-09-30 15:59:46 +0200

    check empty path

--- src/mkdir.sh
+++ src/mkdir.sh
@@ -2,12 +2,21 @@
 
 #------------------------------------------------------------------------------
 # Create directory (including parent directories) if directory does not exists.
-# @abort
+#
+# @param path
+# @require abort
 #------------------------------------------------------------------------------
 function _mkdir {
+
+	if test -z "$1"; then	
+		_abort "Empty directory path"
+	fi
+
 	if ! test -d "$1"; then
 		echo "mkdir -p $1"
-		mkdir -p $1 || _abort "mkdir -p $1"
+		mkdir -p $1 || _abort "mkdir -p '$1'"
+	else
+		echo "directory $1 already exists"
 	fi
 }
 
