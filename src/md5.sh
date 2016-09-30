a76b151ff02459460511c7fc48eecfb607ab4e67
Roland Kujundzic <roland@inkoeln.com>
2016-09-30 17:25:29 +0200

    initial

---
+++ src/md5.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Print md5sum of file.
+#
+# @param file
+# @require abort
+# @print md5sum
+#------------------------------------------------------------------------------
+function _md5 {
+
+	if test -z "$1" || ! test -f "$1"
+	then
+		_abort "No such file [$1]"
+	fi
+
+	local has_md5=`which md5`
+	local md5=
+
+	if test -z "$has_md5"
+	then
+		# on Linux
+		md5=($(md5sum "$1"))
+	else
+		# on OSX
+		md5=`md5 -q "$1"`
+	fi
+
+	echo $md5
+}
