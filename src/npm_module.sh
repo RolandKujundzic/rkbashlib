7ef5f4f68cc1442377bdd3e738d8c7f1527cb860
Roland Kujundzic <roland@inkoeln.com>
2016-09-30 15:04:38 +0200

    initial

---
+++ src/npm_module.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Install npm module $1 (globally if $2 = -g)
+#------------------------------------------------------------------------------
+function _npm_module {
+
+	if test "$2" = "-g"
+	then
+		if test -d /usr/local/lib/node_modules/$1
+		then
+			echo "node module $1 is already globally installed - updating"
+			sudo npm update -g $1
+			return
+		else
+			echo "install node module $1 globally"
+			sudo npm install -g $1
+			return
+		fi
+	fi
+
+	if test -d node_modules/$1
+	then
+		echo "node module $1 is already installed - updating"
+		npm update $1
+	return
+	fi
+
+	npm install $1 $2
+}
+
