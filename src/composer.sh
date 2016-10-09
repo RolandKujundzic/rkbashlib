feafaec7c0a448717fa13a4f1d10f11397181a7b
Roland Kujundzic <roland@inkoeln.com>
2016-10-09 16:20:15 +0200

    allow composer installation in subdirectory

--- src/composer.sh
+++ src/composer.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Install composer
+# Install composer (getcomposer.org)
+#
+# @param install_dir (optional)
 #------------------------------------------------------------------------------
 function _composer {
-	echo "Install compser (getcomposer.org)"
+
+	local INSTALL_DIR=.
+	local CURR=$PWD
+
+	if ! test -z "$1" && test -d "$1"; then
+		INSTALL_DIR=$1
+	fi
+
+  if test -f "$INSTALL_DIR/composer.phar"; then
+		return
+  fi
+
+	echo "Install compser in $INSTALL_DIR"
+	cd $INSTALL_DIR
 	curl -sS https://getcomposer.org/installer | php
+	cd $CURR
 }
 
