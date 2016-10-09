e710d8b0bda5a46cc2fd94cc97cbcd17bdf346c0
Roland Kujundzic <roland@inkoeln.com>
2016-10-09 18:16:42 +0200

    initial

---
+++ src/apigen_doc.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Create apigen documentation for php project.
+#
+# @param source directory (optional, default = src)
+# @param doc directory (optional, default = docs/api)
+# @require composer
+# @require abort
+#------------------------------------------------------------------------------
+function _apigen_doc {
+
+	if ! test -d vender/apigen/apigen; then
+		_composer init
+	fi
+
+	local SRC_DIR=./src
+	local DOC_DIR=./docs/api
+
+	if ! test -z "$1"; then
+		SRC_DIR="$1"
+	fi
+
+	if ! test -z "$2"; then
+		DOC_DIR="$2"
+	fi
+
+	if ! test -d "$SRC_DIR"; then
+		_abort "no such directory [$SRC_DIR]"
+	fi
+
+	if ! test -d "$DOC_DIR"; then
+		_abort "remove existing documentation directory [$DOC_DIR] first"
+	fi
+
+	vendor/apigen/apigen/bin/apigen generate -s "$SRC_DIR" -d "$DOC_DIR"
+}
