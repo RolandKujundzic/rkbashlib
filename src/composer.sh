0cdd1f6639514bf7241d22e89e51df8dc6b67780
Roland Kujundzic <roland@inkoeln.com>
2016-10-09 18:01:28 +0200

    refactoring: parameter empty|init|remove|install

--- src/composer.sh
+++ src/composer.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Install composer (getcomposer.org)
+# Install composer (getcomposer.org). Use init parameter to install apigen/apigen
+# and phpunit/phpunit. Use install if 
 #
-# @param install_dir (optional)
+# @param [|init|remove|install] (optional - default = empty)
 #------------------------------------------------------------------------------
 function _composer {
 
-	local INSTALL_DIR=.
-	local CURR=$PWD
-
-	if ! test -z "$1" && test -d "$1"; then
-		INSTALL_DIR=$1
+	if test "$1" = "remove"; then
+		echo "remove composer"
+		rm -rf composer.phar vendor composer.lock ~/.composer
 	fi
 
-  if test -f "$INSTALL_DIR/composer.phar"; then
-		return
+	if ! test -f composer.phar; then
+		echo "install composer"
+		curl -sS https://getcomposer.org/installer | php
   fi
 
-	echo "Install compser in $INSTALL_DIR"
-	cd $INSTALL_DIR
-	curl -sS https://getcomposer.org/installer | php
-	cd $CURR
+	if test "$1" = "init"; then
+		php composer.phar require --dev apigen/apigen
+		php composer.phar require --dev phpunit/phpunit
+	fi
+
+	if test "$1" = "install" && test -f composer.json; then
+		php composer.phar install
+	fi
 }
 
