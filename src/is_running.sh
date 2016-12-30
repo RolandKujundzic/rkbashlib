504c10033fc253ccd919afe1b286c6ef83041c13
Roland Kujundzic <roland@inkoeln.com>
2016-12-31 00:29:07 +0100

    initial

---
+++ src/is_running.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Abort with error message. Process Expression is either CUSTOM with 
+# regular expression as second parameter (first character must be in brackets)
+# or PORT with port number as second parameter or expression name from list:
+#
+# NGINX, APACHE2, DOCKER_PORT_80, DOCKER_PORT_443 
+#
+# Example:
+#
+# if test "$(_is_running APACHE2)" = "APACHE2_running"; then
+# if test "$(_is_running PORT 80)" != "PORT_running"; then
+# if test "$(_is_running CUSTOM [a]pache2)" = "CUSTOM_running"; then
+#
+# @param Process Expression Name 
+# @param Regular Expression if first parameter is CUSTOM e.g. [a]pache2
+# @return "$1_running"
+#------------------------------------------------------------------------------
+function _is_running {
+
+  if test -z "$1"; then
+    _abort "no process name"
+  fi
+
+  # use [a] = a to ignore "grep process"
+  local APACHE2='[a]pache2.*k start'
+  local DOCKER_PORT_80='[d]ocker-proxy.* -host-port 80'
+  local DOCKER_PORT_443='[d]ocker-proxy.* -host-port 443'
+  local NGINX='[n]ginx.*master process'
+
+  local IS_RUNNING=
+
+  if ! test -z "$2"; then
+		if test "$1" = "CUSTOM"; then
+    	IS_RUNNING=$(ps aux | grep -E "$2")
+		elif test "$1" = "PORT"; then
+			IS_RUNNING=$(netstat -tulpn | grep ":$2")
+		fi
+  elif test -z "${!1}"; then
+    _abort "invalid grep expression name $1 (use NGINX, APACHE2, DOCKER_PORT80, ... or CUSTOM '[n]ame')"
+  else
+    IS_RUNNING=$(ps aux | grep -E "${!1}")
+  fi
+
+  if ! test -z "$IS_RUNNING"; then
+    echo "$1_running"
+  fi
+}
+
