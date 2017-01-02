ca7e123edfbfd4a98a4656e11eb35661dd67357b
Roland Kujundzic <roland@inkoeln.com>
2017-01-02 21:06:57 +0100

    APP_DESC added

--- src/syntax.sh
+++ src/syntax.sh
@@ -2,9 +2,16 @@
 
 #------------------------------------------------------------------------------
 # Abort with SYNTAX: message.
+#
+# @global APP, APP_DESC
 #------------------------------------------------------------------------------
 function _syntax {
 	echo -e "\nSYNTAX: $APP $1\n\n" 1>&2
+
+	if ! test -z "$APP_DESC"; then
+		echo -e "$APP_DESC\n\n" 1>&2
+	fi
+
 	exit 1
 }
 
