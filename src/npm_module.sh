0ae0a8d881b7a72fc906e203c9d7dbfd35e6bbf6
Roland Kujundzic <roland@kujundzic.de>
2017-01-16 11:22:07 +0100

    EXTRA_PARAM added - necessary for ios-deploy on osx

--- src/npm_module.sh
+++ src/npm_module.sh
@@ -7,17 +7,22 @@
 # @param npm_param (e.g. -g, --save-dev)
 #------------------------------------------------------------------------------
 function _npm_module {
+	local EXTRA_PARAM=
+
+	if test "$1" = "ios-deploy"; then
+		EXTRA_PARAM="--unsafe-perm=true --allow-root"
+	fi
 
 	if test "$2" = "-g"
 	then
 		if test -d /usr/local/lib/node_modules/$1
 		then
 			echo "node module $1 is already globally installed - updating"
-			sudo npm update -g $1
+			sudo npm update $EXTRA_PARAM -g $1
 			return
 		else
 			echo "install node module $1 globally"
-			sudo npm install -g $1
+			sudo npm install $EXTRA_PARAM -g $1
 			return
 		fi
 	fi
@@ -25,10 +30,9 @@ function _npm_module {
 	if test -d node_modules/$1
 	then
 		echo "node module $1 is already installed - updating"
-		npm update $1
+		npm update $EXTRA_PARAM $1
 	return
 	fi
 
-	npm install $1 $2
+	npm install $EXTRA_PARAM $1 $2
 }
-
