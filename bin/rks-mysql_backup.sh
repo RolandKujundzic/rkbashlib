3d72b1043582fc6067429737edeefe593672e8da
Roland Kujundzic <roland@inkoeln.com>
2017-04-20 11:09:32 +0200

    _mysql_backup changed

--- bin/rks-mysql_backup.sh
+++ bin/rks-mysql_backup.sh
@@ -122,33 +122,43 @@ function _mysql_dump {
 # 10 8,9,10,11,12,13,14,15,16,17,18,19,20  * * *  /path/to/mysql_backup.sh
 #
 # @param backup directory
+# @global MYSQL_CONN mysql connection string "-h DBHOST -u DBUSER -pDBPASS DBNAME"
 # @require abort, cd, cp, mysql_dump
 #------------------------------------------------------------------------------
 function _mysql_backup {
 
-	local LOCK=mysql_backup.lock
-	local MIN_SUFFIX=`date +".%H%M.sql"`
-	local DAY_SUFFIX=`date +".%Y%m%d.sql"`
-	local DUMP="mysql_dump$MIN_SUFFIX"".tar.gz"
-	local DAILY_DUMP="mysql_dump$DAY_SUFFIX"".tar.gz"
+	local DUMP="mysql_dump."`date +"%H%M"`".tgz"
+	local DAILY_DUMP="mysql_dump."`date +"%Y%m%d"`".tgz"
+	local FILES="'tables.txt'"
 
-	if test -f "$LOCK"; then
+	if test -f "tables.txt"; then
 		_abort "last dump failed or is still running"
 	fi
 
 	_cd $1
 
 	echo "update $DUMP and $DAILY_DUMP" > $LOCK
-	_mysql_dump "mysql_create$MIN_SUFFIX" "-d"
-	_mysql_dump "mysql_insert$MIN_SUFFIX" "--no-create-info=TRUE"
+
+	# dump structure
+	echo "create_tables.sql" > tables.txt
+	_mysql_dump "create_tables.sql" "-d"
+	FILES="$FILES 'create_tables.sql'"
+
+	for T in $(mysql $MYSQL_CONN -e 'show tables' -s --skip-column-names)
+	do
+		# dump table
+		echo $T >> tables.txt
+		_mysql_dump "$T"".sql" "--extended-insert=FALSE --no-create-info=TRUE"
+		FILES="$FILES '$T'"
+	done
 
 	echo "archive database dump as $DUMP"
-	tar -czf "$DUMP" "mysql_create$MIN_SUFFIX" "mysql_insert$MIN_SUFFIX" || _abort "tar -czf '$DUMP' failed"
+	tar -czf "$DUMP" $FILES || _abort "tar -czf '$DUMP' $FILES failed"
 
 	_cp "$DUMP" "$DAILY_DUMP"
 
-	# cleanump
-	rm "mysql_create$MIN_SUFFIX" "mysql_insert$MIN_SUFFIX" "$LOCK"
+	# cleanup
+	rm $FILES
 
 	_cd
 }
