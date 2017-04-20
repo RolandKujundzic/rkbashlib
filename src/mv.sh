add2e879a00859598a8e2075519d9eb42dbb5c89
Roland Kujundzic <roland.kujundzic@ogulo.de>
2017-04-20 14:29:29 +0200

    initial

---
+++ src/mv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Move files/directories. Target path directory must exist.
+#
+# @param source_path
+# @param target_path
+# @require abort
+#------------------------------------------------------------------------------
+function _mv {
+
+	if test -z "$1"; then
+		_abort "Empty source path"
+	fi
+
+	if test -z "$2"; then
+		_abort "Empty target path"
+	fi
+
+	local PDIR=`dirname "$2"`
+	if ! test -d "$PDIR"; then
+		_abort "No such directory [$PDIR]"
+	fi
+
+	echo "mv '$1' '$2'"
+	mv "$1" "$2" || _abort "mv '$1' '$2' failed"
+}
+
