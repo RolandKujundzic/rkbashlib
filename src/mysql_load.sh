5fc8af70304ffe27304928bffabd2124c3ea5081
Roland Kujundzic <roland@kujundzic.de>
2017-04-21 03:08:14 +0200

    restore.sh added

--- src/mysql_load.sh
+++ src/mysql_load.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Load mysql dump. Abort if error.
+# Load mysql dump. Abort if error. If restore.sh exists append load command to 
+# restore.sh
 #
 # @param dump_file
 # @global MYSQL_CONN mysql connection string "-h DBHOST -u DBUSER -pDBPASS DBNAME"
@@ -23,9 +24,14 @@ function _mysql_load {
 		_abort "invalid mysql dump [$1]"
 	fi
 
-	echo "mysql ... < $1"
-	SECONDS=0
-	mysql $MYSQL_CONN < "$1" || _abort "mysql ... < $1 failed"
-	echo "$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds elapsed."
+	if test -f "restore.sh"; then
+		echo "add $1 to restore.sh"
+		echo "mysql $MYSQL_CONN < $1 &" >> restore.sh
+	else
+		echo "mysql ... < $1"
+		SECONDS=0
+		mysql $MYSQL_CONN < "$1" || _abort "mysql ... < $1 failed"
+		echo "$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds elapsed."
+	fi
 }
 
