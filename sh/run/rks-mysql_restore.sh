03d7464b26e583e58e157dee7f076c9f4f94a5e1
Roland Kujundzic <roland.kujundzic@ogulo.de>
2017-04-26 16:36:20 +0200

    try to restore todays backup first, then yesterdays

--- sh/run/rks-mysql_restore.sh
+++ sh/run/rks-mysql_restore.sh
@@ -6,5 +6,14 @@
 
 MYSQL_CONN="-h localhost -u DBUSER -pDBPASS DBNAME"
 
-_mysql_restore /path/to/mysql_dump.sql
+BACKUP_TODAY="/path/to/mysql_dump."`date +"%Y%m%d"`".tgz"
+BACKUP_YESTERDAY="/path/to/mysql_dump."`date --date='-1 day' +"%Y%m%d"`".tgz"
+
+if test -f "$BACKUP_TODAY"; then
+  _mysql_restore "$BACKUP_TODAY" 1
+elif test -f "$BACKUP_YESTERDAY"; then
+  _mysql_restore "$BACKUP_YESTERDAY" 1
+else
+  _abort "neither yesterdays ($BACKUP_YESTERDAY) nor todays ($BACKUP_TODAY) backup found"
+fi
 
