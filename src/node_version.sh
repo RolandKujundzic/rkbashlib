87d6d4106764654e2d0f4d05b0952a5e4b5f0f8b
Roland Kujundzic <roland@inkoeln.com>
2017-05-24 11:53:19 +0200

    backup current version to /opt

--- src/node_version.sh
+++ src/node_version.sh
@@ -3,8 +3,8 @@
 #------------------------------------------------------------------------------
 # Check node.js version. Update to NPM_VERSION
 #
-# @global NODE_VERSION, NPM_VERSION
-# @require ver3, abort, require_global
+# @global NODE_VERSION NPM_VERSION APP_PREFIX APP_FILE_LIST APP_DIR_LIST
+# @require ver3 abort require_global install_app mkdir cp dl_unpack md5 os_type
 #------------------------------------------------------------------------------
 function _node_version {
 	_require_global "NODE_VERSION NPM_VERSION"
@@ -12,7 +12,24 @@ function _node_version {
 	local CURR_NODE_VERSION=`node --version`
 	if [ $(_ver3 $CURR_NODE_VERSION) -lt $(_ver3 $NODE_VERSION) ]
 	then
-		_abort "Update node.js to version >= $NODE_VERSION - see https://nodejs.org/"
+		local OS_TYPE=$(_os_type)
+
+		if test -f /usr/local/bin && test "$OS_TYPE" = "linux"
+		then
+			APP_FILE_LIST="bin/npm bin/node share/man/man1/node.1 share/systemtap/tapset/node.stp"
+			APP_DIR_LIST="include/node lib/node_modules share/doc/node"
+
+			if test -z "$CURR_NODE_VERSION" && test -f /usr/local/bin/node; then
+				APP_PREFIX="/opt/node_$CURR_NODE_VERSION"
+				echo "backup current node version to $APP_PREFIX"
+				_install_app .
+			if
+
+			APP_PREFIX="/usr/local"
+			_install_app "node-$NODE_VERSION-linux-x64" "https://nodejs.org/dist/$NODE_VERSION/node-$NODE_VERSION-linux-x64.tar.xz"
+		else
+			_abort "Update node.js to version >= $NODE_VERSION - see https://nodejs.org/"
+		fi
 	fi
 
 	local CURR_NPM_VERSION=`npm --version`
