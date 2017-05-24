c574dc3a27a262493f2e8db1d1259a544a1d7621
Roland Kujundzic <roland@inkoeln.com>
2017-05-24 11:34:00 +0200

    include dl_unpack, update @require

--- src/install_app.sh
+++ src/install_app.sh
@@ -3,10 +3,14 @@
 #------------------------------------------------------------------------------
 # Install files from APP_FILE_LIST and APP_DIR_LIST to APP_PREFIX.
 #
-# global APP_PREFIX, APP_FILE_LIST, APP_DIR_LIST
-# require mkdir cp
+# @param string app dir
+# @param string app url
+# @global APP_PREFIX, APP_FILE_LIST, APP_DIR_LIST
+# @require mkdir cp dl_unpack abort md5
 #------------------------------------------------------------------------------
 function _install_app {
+
+	_dl_unpack $1 $2
 
   if ! test -d $APP_PREFIX; then
     _mkdir $APP_PREFIX
@@ -15,13 +19,15 @@ function _install_app {
   for FILE in $APP_FILE_LIST
   do
     _mkdir `dirname $APP_PREFIX/$FILE`
-    _cp $FILE $APP_PREFIX/$FILE md5
+    _cp $1/$FILE $APP_PREFIX/$FILE md5
   done
 
   for DIR in $APP_DIR_LIST
   do
     _mkdir `dirname $APP_PREFIX/$DIR`
-    _cp $DIR $APP_PREFIX/$DIR
+    _cp $1/$DIR $APP_PREFIX/$DIR
   done
+
+	_rm $1
 }
 
