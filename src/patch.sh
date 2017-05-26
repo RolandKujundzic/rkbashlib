b06a9ff25278cda596dbc6b7aa029fc5359c1fee
Roland Kujundzic <roland@inkoeln.com>
2017-05-26 10:07:36 +0200

    initial

---
+++ src/patch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Patch if $1/patch.sh exists. Example patch.sh:
+#
+# PATCH_LIST=MainViewController.m
+# PATCH_DIR=platforms/ios
+#
+# @param directory path
+#------------------------------------------------------------------------------
+function _patch {
+
+	if ! test -f "$1/patch.sh"; then
+		return
+	fi
+
+	. $1/patch.sh
+
+  for a in $PATCH_LIST
+  do
+    local SRC=`find $PATCH_DIR | grep $a`
+
+    if test -f $1/$a.patch && test -f "$SRC"
+    then
+			echo "patch $SRC $1/$a.patch"
+      patch $SRC $1/$a.patch || _abort "patch failed"
+    fi
+  done
+}
+
