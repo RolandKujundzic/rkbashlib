69749b2ab31e9021eaf3b5794efe6bdedb29259a
Roland Kujundzic <roland.kujundzic@ogulo.de>
2017-06-08 16:23:20 +0200

    check for macos first

--- src/os_type.sh
+++ src/os_type.sh
@@ -6,13 +6,13 @@
 # @print string
 #------------------------------------------------------------------------------
 function _os_type {
-	if test "$OSTYPE" = "linux-gnu"; then
-		echo "linux"
-	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-		echo "linux"
-	elif [ "$(uname)" == "Darwin" ]; then
+	if [ "$(uname)" = "Darwin" ]; then
 		echo "macos"        
-	elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+	elif [ test "$OSTYPE" = "linux-gnu" ]; then
+		echo "linux"
+	elif [ $(expr substr $(uname -s) 1 5) = "Linux" ]; then
+		echo "linux"
+	elif [ $(expr substr $(uname -s) 1 5) = "MINGW" ]; then
 		echo "cygwin"
 	fi
 }
