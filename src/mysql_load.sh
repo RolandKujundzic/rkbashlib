39ce20d461fed9ad9417c62d9004cffec0d1c366
Roland Kujundzic <roland@inkoeln.com>
2017-07-19 12:04:22 +0200

    autodetect sql dump

--- src/mysql_load.sh
+++ src/mysql_load.sh
@@ -2,36 +2,54 @@
 
 #------------------------------------------------------------------------------
 # Load mysql dump. Abort if error. If restore.sh exists append load command to 
-# restore.sh
+# restore.sh. If MYSQL_CONN is empty but DB_NAME and DB_PASS exist use these.
 #
-# @param dump_file
+# @param dump_file (if empty try data/sql/mysqlfulldump.sql, setup/mysqlfulldump.sql)
 # @global MYSQL_CONN mysql connection string "-h DBHOST -u DBUSER -pDBPASS DBNAME"
 # @abort
-# @require abort
+# @require abort confirm
 #------------------------------------------------------------------------------
 function _mysql_load {
 
 	if test -z "$MYSQL_CONN"; then
-		_abort "mysql connection string MYSQL_CONN is empty"
+		if ! test -z "$DB_NAME" && ! test -z "$DB_PASS"; then
+			MYSQL_CONN="-h localhost -u $DB_NAME -p$DB_PASS $DB_NAME"
+		else
+			_abort "mysql connection string MYSQL_CONN is empty"
+		fi
 	fi
 
-	if ! test -f "$1"; then
-		_abort "no such mysql dump [$1]"
+	local DUMP=$1
+
+	if ! test -f "$DUMP"; then
+		if test -s "data/sql/mysqlfulldump.sql"; then
+			DUMP=data/sql/mysqlfulldump.sql
+		elif test -s "setup/mysqlfulldump.sql"; then
+			DUMP=setup/mysqlfulldump.sql
+		else
+			_abort "no such mysql dump [$DUMP]"
+		fi
+
+		_confirm "Load $DUMP?"
+		if test "$CONFIRM" != "y"; then
+			echo "Do not load $DUMP"
+			return
+		fi
 	fi
 
-	local DUMP_OK=`tail -1 "$1" | grep "Dump completed"`
+	local DUMP_OK=`tail -1 "$DUMP" | grep "Dump completed"`
 	if test -z "$DUMP_OK"; then
-		_abort "invalid mysql dump [$1]"
+		_abort "invalid mysql dump [$DUMP]"
 	fi
 
 	if test -f "restore.sh"; then
-		local LOG="$1"".log"
-		echo "add $1 to restore.sh"
-		echo "mysql $MYSQL_CONN < $1 &> $LOG && rm $1 &" >> restore.sh
+		local LOG="$DUMP"".log"
+		echo "add $DUMP to restore.sh"
+		echo "mysql $MYSQL_CONN < $DUMP &> $LOG && rm $DUMP &" >> restore.sh
 	else
-		echo "mysql ... < $1"
+		echo "mysql ... < $DUMP"
 		SECONDS=0
-		mysql $MYSQL_CONN < "$1" || _abort "mysql ... < $1 failed"
+		mysql $MYSQL_CONN < "$DUMP" || _abort "mysql ... < $DUMP failed"
 		echo "$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds elapsed."
 	fi
 }
