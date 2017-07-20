63b76a9bace3251606c0e2a380f4ad87dce3892b
Roland Kujundzic <roland@inkoeln.com>
2017-07-20 11:11:02 +0200

    fix if condition

--- src/recreate_docker_db.sh
+++ src/recreate_docker_db.sh
@@ -3,15 +3,14 @@
 #------------------------------------------------------------------------------
 # Re-create database if inside docker.
 #
-# @param string website directory
 # @require abort confirm mysql_split_dsn mysql_create_db mysql_load
 # @export DB_NAME DB_PASS MYSQL_CONN
 #------------------------------------------------------------------------------
 function _recreate_docker_db {
   local INSIDE_DOCKER=`cat /etc/hosts | grep 172.17`
 
-	if ! test -z "$INSIDE_DOCKER"; then
-		echo "no inside docker - abort database recreate"
+	if test -z "$INSIDE_DOCKER"; then
+		echo "not inside docker - abort database recreate"
 		return
   fi
 
