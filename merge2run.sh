4ffe1a7a11d1ea3f440476772c4b33cdc4381c86
Roland Kujundzic <roland@inkoeln.com>
2017-10-13 00:44:36 +0200

    SCAN_INCLUDE=1 added

--- merge2run.sh
+++ merge2run.sh
@@ -4,15 +4,22 @@
 #
 
 SCRIPT_NAME="$0"
-
 command -v realpath > /dev/null 2>&1 && SCRIPT_NAME=`realpath "$0"`
 
-SCRIPT_DIR=`dirname "$SCRIPT_NAME"`
+SCRIPT_SRC=`dirname "$SCRIPT_NAME"`"/src"
 
-. "$SCRIPT_DIR/src/abort.sh"
-. "$SCRIPT_DIR/src/syntax.sh"
-. "$SCRIPT_DIR/src/confirm.sh"
-. "$SCRIPT_DIR/src/rm.sh"
+. "$SCRIPT_SRC/abort.sh"
+. "$SCRIPT_SRC/syntax.sh"
+. "$SCRIPT_SRC/confirm.sh"
+. "$SCRIPT_SRC/require_global.sh"
+. "$SCRIPT_SRC/required_rkscript.sh"
+. "$SCRIPT_SRC/scan_rkscript_src.sh"
+. "$SCRIPT_SRC/cd.sh"
+. "$SCRIPT_SRC/rm.sh"
+
+if test -z "$RKSCRIPT_PATH"; then
+	RKSCRIPT_PATH=`dirname "$SCRIPT_NAME"`
+fi
 
 APP=$0
 OUT=run.sh
@@ -31,6 +38,20 @@ else
 	if test -f "sh/run/merge2$1"; then
 		. sh/run/merge2$1
 		OUT=$1
+
+		if ! test -z "$SCAN_INCLUDE"; then
+			_scan_rkscript_src
+			INCLUDE=
+
+			for a in $MERGE2RUN; do
+				_required_rkscript "sh/run/$a"".sh" 1
+				echo "scan include sh/run/$a: $REQUIRED_RKSCRIPT"
+				INCLUDE="$REQUIRED_RKSCRIPT $INCLUDE"
+			done
+
+			INCLUDE=`echo "$INCLUDE" | sed -e "s/ /\n/g" | sed -e "s/^_//g" | xargs`
+			MERGE2RUN="$INCLUDE $MERGE2RUN"
+		fi
 	else
 		MERGE2RUN="$1"
 	fi
@@ -52,7 +73,7 @@ echo -e "#!/bin/bash\nMERGE2RUN=\"$MERGE2RUN\"\n" > $OUT
 
 for a in $MERGE2RUN
 do
-	FUNC="$SCRIPT_DIR/src/$a.sh"
+	FUNC="$SCRIPT_SRC/$a.sh"
 
 	if test -f "sh/run/$a.sh"; then
 		FUNC="sh/run/$a.sh"
