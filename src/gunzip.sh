9e154b903b20c3b417562f98d70669faea4d5482
Roland Kujundzic <roland@kujundzic.de>
2018-01-18 21:28:17 +0100

    initial

---
+++ src/gunzip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Gunzip file.
+#
+# @param file
+# @param ignore_if_not_gzip (optional)
+# @require _abort
+#------------------------------------------------------------------------------
+function _gunzip {
+
+	if ! test -f "$1"; then
+		_abort "no such gzip file [$1]"
+	fi
+
+	local IS_GZIP=`file "$1"  | grep 'gzip compressed data'`
+
+	if test -z "$IS_GZIP"; then
+		if test -z "$2"; then
+			_abort "invalid gzip file [$1]"
+		else 
+			echo "$1 is not in gzip format - skip gunzip"
+			return
+		fi
+	fi
+
+	echo "gunzip $1"
+	gunzip "$1"
+}
+
