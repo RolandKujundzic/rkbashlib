5dcad204b79c1c2d245ad515f3408c514ba0b29b
Roland Kujundzic <roland@inkoeln.com>
2018-03-27 16:21:47 +0200

    hide error

--- src/ip_address.sh
+++ src/ip_address.sh
@@ -10,7 +10,7 @@ function _ip_address {
 	IP_ADDRESS=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 	local HOST=`hostname`
-	local PING_OK=`ping -c 1 $HOST | grep $IP_ADDRESS`
+	local PING_OK=`ping -c 1 $HOST 2> /dev/null | grep $IP_ADDRESS`
 
 	if test -z "$PING_OK"; then
 		_abort "failed to detect IP_ADDRESS (ping -c 1 $HOST != $IP_ADDRESS)"
