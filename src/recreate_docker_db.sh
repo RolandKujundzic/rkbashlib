a57b11d85f060900d3fa2504f568f17d5e266aef
Roland Kujundzic <roland@kujundzic.de>
2018-04-11 21:54:16 +0200

    update

--- src/recreate_docker_db.sh
+++ src/recreate_docker_db.sh
@@ -3,19 +3,23 @@
 #------------------------------------------------------------------------------
 # Re-create database if inside docker.
 #
+# @param do_not_load_dump (optional, default = empty = load_dump)
 # @require _mysql_split_dsn _mysql_create_db _mysql_load
 # @export DB_NAME DB_PASS MYSQL_CONN
 #------------------------------------------------------------------------------
 function _recreate_docker_db {
-  local INSIDE_DOCKER=`cat /etc/hosts | grep 172.17`
+	local INSIDE_DOCKER=`cat /etc/hosts | grep 172.17`
 
 	if test -z "$INSIDE_DOCKER"; then
 		echo "not inside docker - abort database recreate"
 		return
-  fi
+	fi
 
 	_mysql_split_dsn
 	_mysql_create_db $DB_NAME $DB_PASS
-	_mysql_load
+
+	if test -z "$1"; then
+		_mysql_load
+	fi
 }
 
