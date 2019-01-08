febf1eb77495d5abcd55c6b061a7cf000c5fdb10
Roland Kujundzic <roland@inkoeln.com>
2019-01-08 04:28:00 +0100

    initial

---
+++ src/check_ip.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Abort if ip_address does not point to IP_ADDRESS
+#
+# @global IP_ADDRESS
+# @param ip_address
+# @require _abort
+#------------------------------------------------------------------------------
+function _check_ip {
+	local IP_OK=`ping -c 1 "$1" 2> /dev/null | grep "$IP_ADDRESS"`
+
+	if test -z "$IP_OK"; then
+		_abort "$1 does not point to server ip $IP_ADDRESS"
+	fi
+}
+
+
