1b63b1e659d4f51997686eff66d312e454a7e0c4
Roland Kujundzic <roland@inkoeln.com>
2019-01-08 04:34:47 +0100

    added _abort parameter

--- src/check_ssl.sh
+++ src/check_ssl.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Print ssl public key status (in /etc/letsencrypt/live/).
+# Print ssl public key status (in /etc/letsencrypt/live/) and $2 is empty.
+# Abort if $2 is set and status != valid.
 # 
 # @export ENDDATE
 # @param string domain
-# @print valid, missing or expired
+# @param int abort (_abort if set)
+# @print valid, missing or expired (if $2 is empty)
+# @require _abort
 #------------------------------------------------------------------------------
 function _check_ssl {
-  if ! test -f /etc/letsencrypt/live/$1/fullchain.pem; then
-    echo "missing"
-    return
-  fi
+	if ! test -f /etc/letsencrypt/live/$1/fullchain.pem; then
+		if ! test -z "$2"; then
+			_abort "missing file /etc/letsencrypt/live/$1/fullchain.pem"
+		fi
 
-  ENDDATE=`openssl x509 -enddate -noout -in /etc/letsencrypt/live/$1/fullchain.pem`
-  export ENDDATE=${ENDDATE:9}
-  local STATUS=$(php -r 'print strtotime(getenv("ENDDATE")) > time() + 3600 * 24 * 14 ? "valid" : "expired";')
-  echo $STATUS
+		echo "missing"
+		return
+	fi
+
+	ENDDATE=`openssl x509 -enddate -noout -in /etc/letsencrypt/live/$1/fullchain.pem`
+	export ENDDATE=${ENDDATE:9}
+	local STATUS=$(php -r 'print strtotime(getenv("ENDDATE")) > time() + 3600 * 24 * 14 ? "valid" : "expired";')
+
+	if test "$STATUS" = "expired"; then
+		_abort "domain $1 in /etc/letsencrypt/live/$1/fullchain.pem has expired"
+	fi
+
+	echo $STATUS
 }
 
