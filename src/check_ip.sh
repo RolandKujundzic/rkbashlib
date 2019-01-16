ed17cf287dc49ca49ef7e740d1beb07aba4ceb3a
Roland Kujundzic <roland@inkoeln.com>
2019-01-16 12:49:39 +0100

    use PING4

--- src/check_ip.sh
+++ src/check_ip.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Abort if ip_address does not point to IP_ADDRESS
+# Abort if ip_address does not point to IP_ADDRESS.
+# Call _ip_address first.
 #
-# @global IP_ADDRESS
+# @global IP_ADDRESS, PING4
 # @param ip_address
 # @require _abort
 #------------------------------------------------------------------------------
 function _check_ip {
-	local IP_OK=`ping4 -c 1 "$1" 2> /dev/null | grep "$IP_ADDRESS"`
+	local IP_OK=`$PING4 -c 1 "$1" 2> /dev/null | grep "$IP_ADDRESS"`
 
 	if test -z "$IP_OK"; then
 		_abort "$1 does not point to server ip $IP_ADDRESS"
--- src/ip_address.sh
+++ src/ip_address.sh
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 # Export ip address
 #
-# @export IP_ADDRESS
+# @export IP_ADDRESS, PING4
 # @require _abort
 #------------------------------------------------------------------------------
 function _ip_address {
@@ -13,11 +13,17 @@ function _ip_address {
 		IP_ADDRESS=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 	fi
 
+	PING4="ping4"
+	local HAS_PING4=`which $PING4`
+	if test -z "$HAS_PING4"; then
+		PING4="ping"
+	fi
+
 	local HOST=`hostname`
 	local PING_OK=`ping4 -c 1 $HOST 2> /dev/null | grep $IP_ADDRESS`
 
 	if test -z "$PING_OK"; then
-		_abort "failed to detect IP_ADDRESS (ping4 -c 1 $HOST != $IP_ADDRESS)"
+		_abort "failed to detect IP_ADDRESS ($PING4 -c 1 $HOST != $IP_ADDRESS)"
 	fi
 }
 
