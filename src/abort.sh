eef4ca7458464462657db98b7f1e3e4ef4fdbdd7
Roland Kujundzic <roland@inkoeln.com>
2019-01-21 11:31:31 +0100

    allow NO_ABORT

--- src/abort.sh
+++ src/abort.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Abort with error message.
+# Abort with error message. Use NO_ABORT=1 for just warning output.
 #
 # @exit
-# @global APP
+# @global APP, NO_ABORT
 # @param abort message
 #------------------------------------------------------------------------------
 function _abort {
+	if test "$NO_ABORT" = 1; then
+		echo "WARNING: $1"
+		return
+	fi
+
 	echo -e "\nABORT: $1\n\n" 1>&2
 	# make shure APP dies even if _abort is called from subprocess
   kill $(ps aux | grep "$APP" | awk '{print $2}')
