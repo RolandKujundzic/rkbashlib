f463aa2ab54ec5a4a9c1033eb98c2fd32c424279
Roland Kujundzic <roland@inkoeln.com>
2019-01-21 11:01:42 +0100

    update

--- src/abort.sh
+++ src/abort.sh
@@ -3,10 +3,14 @@
 #------------------------------------------------------------------------------
 # Abort with error message.
 #
+# @exit
+# @global APP
 # @param abort message
 #------------------------------------------------------------------------------
 function _abort {
 	echo -e "\nABORT: $1\n\n" 1>&2
+	# make shure APP dies even if _abort is called from subprocess
+  kill $(ps aux | grep "$APP" | awk '{print $2}')
 	exit 1
 }
 
