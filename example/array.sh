dffc2c9bc55e4b48f9417d69d01c53d2accbdf6b
Roland Kujundzic <roland@inkoeln.com>
2019-01-21 12:00:00 +0100

    update

--- example/array.sh
+++ example/array.sh
@@ -5,7 +5,7 @@ function test {
 	echo -e "\ntest: \$1=[$1]"
 
 	local ARRAY=( $1 )
-	echo "1:1=[${ARRAY[@]:1:1}] 1:2=[${ARRAY[@]:1:2}] :3=[${ARRAY[@]:3}]"
+	echo "1:1=[${ARRAY[@]:1:1}] 0:2=[${ARRAY[@]:0:2}] :3=[${ARRAY[@]:3}]"
 	# the space before -3|-2 is IMPORTANT!
 	echo "-3:1=[${ARRAY[@]: -3:1}] -2:2=[${ARRAY[@]: -2:2}]"
 	echo
