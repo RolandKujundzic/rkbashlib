0154365efe2d0d26317047fa12dbec781899aee6
Roland Kujundzic <roland@inkoeln.com>
2019-01-31 13:52:46 +0100

    initial

---
+++ src/rkscript.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+function __abort {
+	echo -e "\nABORT: $1\n\n"
+	exit 1
+}
+
+
+#------------------------------------------------------------------------------
+# Use for dynamic loading.
+# @example _rkscript "_rm _mv _cp _mkdir"
+# @global RKSCRIPT = /path/to/rkscript/src
+# @param function list
+#------------------------------------------------------------------------------
+function _rkscript {
+
+	if test -z "$RKSCRIPT"; then
+		RKSCRIPT=../../rkscript/src
+	fi
+
+	if ! test -d "$RKSCRIPT"; then
+		RKSCRIPT=../../../rkscript/src
+	fi
+
+	local ABORT=_abort
+	local HAS_ABORT=`type -t $ABORT`
+	if ! test "$HAS_ABORT" = "function"; then
+		ABORT=__abort
+	fi
+
+	if ! test -d "$RKSCRIPT" || ! test -f "$RKSCRIPT/abort.sh"; then
+		$ABORT "invalid RKSCRIPT path - use RKSCRIPT=/path/to/rkscript/src $APP"
+	fi
+
+	for a in $1; do
+		local TYPE=`type -t $a`
+		if ! test "$TYPE" = "function"; then
+			echo "load $a"
+			. "$RKSCRIPT/${a:1}.sh" || $ABORT "no such function $a"
+		else 
+			echo "found $a"
+		fi
+	done
+}
+
