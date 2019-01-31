878b0fee63e95556a8e5d13563476bc01f33fbdb
Roland Kujundzic <roland@inkoeln.com>
2019-01-31 17:52:14 +0100

    update

--- src/rkscript.sh
+++ src/rkscript.sh
@@ -30,7 +30,7 @@ function _rkscript {
 	fi
 
 	if ! test -d "$RKSCRIPT" || ! test -f "$RKSCRIPT/abort.sh"; then
-		$ABORT "invalid RKSCRIPT path [$RKSCRIPT] - $APP"
+		$ABORT "invalid RKSCRIPT path [$RKSCRIPT] - $APP_PREFIX $APP"
 	fi
 
 	for a in $1; do
--- src/syntax.sh
+++ src/syntax.sh
@@ -4,11 +4,15 @@
 # Abort with SYNTAX: message.
 # Usually APP=$0
 #
-# @global APP, APP_DESC
+# @global APP, APP_DESC, $APP_PREFIX
 # @param message
 #------------------------------------------------------------------------------
 function _syntax {
-	echo -e "\nSYNTAX: $APP $1\n" 1>&2
+	if ! test -z "$APP_PREFIX"; then
+		echo -e "\nSYNTAX: $APP_PREFIX $APP $1\n" 1>&2
+	else
+		echo -e "\nSYNTAX: $APP $1\n" 1>&2
+	fi
 
 	if ! test -z "$APP_DESC"; then
 		echo -e "$APP_DESC\n\n" 1>&2
