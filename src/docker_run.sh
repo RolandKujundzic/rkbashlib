6ede0557b53beaf684253b31dae23b8b97b70000
Roland Kujundzic <roland@inkoeln.com>
2019-07-24 11:12:37 +0200

    initial

---
+++ src/docker_run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Remove stopped docker container $1 (if found). Start docker container $1.
+#
+# @param name
+# @param config file
+# @require _abort _cd _docker_stop _docker_rm
+#------------------------------------------------------------------------------
+function _docker_run {
+	_docker_rm $1
+
+	if ! test -z "$WORKSPACE" && ! test -z "$CURR" && test -d "$WORKSPACE/linux/rkdocker"; then
+		_cd "$WORKSPACE/linux/rkdocker"
+	else
+		_abort "Export WORKSPACE (where $WORKSPACE/linux/rkdocker exists) and CURR=path/current/directory"
+	fi
+
+	local CONFIG=
+
+	if test -f "$CURR/$2"; then
+		CONFIG="$CURR/$2"
+	elif test -f "$2"; then
+		CONFIG="$2"
+	else
+		_abort "No such configuration $CURR/$2 ($PWD/$2)"
+	fi
+	
+  echo "DOCKER_NAME=$1 ./run.sh $CONFIG start"
+  DOCKER_NAME=$1 ./run.sh $2 start
+
+	cd $CURR
+}
+
