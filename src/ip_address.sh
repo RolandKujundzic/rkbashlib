252a69b132a92d0cc5e832dc2027d79a5d061b4f
Roland Kujundzic <roland@inkoeln.com>
2019-08-19 10:15:56 +0200

    update

--- src/ip_address.sh
+++ src/ip_address.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Export ip address
+# Export ip address as IP_ADDRESS (ip4) and IP6_ADDRESS (ip6).
 #
 # @export IP_ADDRESS, IP6_ADDRESS
 # @require _abort _require_program
 #------------------------------------------------------------------------------
 function _ip_address {
 	_require_program ip
-	IP_ADDRESS=`ip route get 1 | awk '{print $NF;exit}'`
+
+	IP_ADDRESS=`ip route get 1 | grep -E ' src [0-9\.]+ uid ' | sed -e 's/.* src //' | sed -e 's/ uid.*//'`
 	IP6_ADDRESS=`ip -6 addr | grep 'scope global' | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d'`
 
-	if test -z "$IP_ADDRESS" || test "$IP_ADDRESS" = "0"; then
+	if test -z "$IP_ADDRESS"; then
 		IP_ADDRESS=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 	fi
 
 	_require_program ping
-	local HOST=`hostname`
-	local PING_OK=`ping -4 -c 1 $HOST 2> /dev/null | grep $IP_ADDRESS`
+	local host=`hostname`
+	local ping_ok=`ping -4 -c 1 $host 2> /dev/null | grep $IP_ADDRESS`
 
-	if test -z "$PING_OK"; then
-		_abort "failed to detect IP_ADDRESS (ping -4 -c 1 $HOST != $IP_ADDRESS)"
+	if test -z "$ping_ok"; then
+		_abort "failed to detect IP_ADDRESS (ping -4 -c 1 $host != $IP_ADDRESS)"
 	fi
 }
 
