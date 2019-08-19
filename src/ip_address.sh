e06a7d545cb3227910dd97800efb87d00e9c93a2
Roland Kujundzic <roland@inkoeln.com>
2019-08-19 10:44:00 +0200

    export DYNAMIC_IP

--- src/ip_address.sh
+++ src/ip_address.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Export ip address as IP_ADDRESS (ip4) and IP6_ADDRESS (ip6).
+# Export ip address as IP_ADDRESS (ip4) and IP6_ADDRESS (ip6) (and DYNAMIC_IP).
 #
-# @export IP_ADDRESS, IP6_ADDRESS
+# @export IP_ADDRESS, IP6_ADDRESS, DYNAMIC_IP
 # @require _abort _require_program
 #------------------------------------------------------------------------------
 function _ip_address {
 	_require_program ip
 
 	IP_ADDRESS=`ip route get 1 | grep -E ' src [0-9\.]+ uid ' | sed -e 's/.* src //' | sed -e 's/ uid.*//'`
-	IP6_ADDRESS=`ip -6 addr | grep 'scope global' | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d'`
-
 	if test -z "$IP_ADDRESS"; then
 		IP_ADDRESS=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 	fi
 
+	IP6_ADDRESS=`ip -6 addr | grep 'scope global' | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d'`
+	local ip6_dyn=`ip -6 addr | grep 'scope global temporary dynamic' | awk '{print $2}' | sed -e 's/\/[0-9]*$//'`
+	if ! test -z "$ip6_dyn"; then
+		IP6_ADDRESS="$ip6_dyn"
+		DYNAMIC_IP=1
+	fi
+
 	_require_program ping
 	local host=`hostname`
-	local ping_ok=`ping -4 -c 1 $host 2> /dev/null | grep $IP_ADDRESS`
+	local ping_ok=`ping -4 -c 1 "$host" 2>/dev/null | grep "$IP_ADDRESS"`
 
 	if test -z "$ping_ok"; then
-		_abort "failed to detect IP_ADDRESS (ping -4 -c 1 $host != $IP_ADDRESS)"
+		ping_ok=`ping -4 -c 1 "$host" 2>/dev/null | grep "127.0."`
+
+		if test -z "$ping_ok"; then
+			_abort "failed to detect IP_ADDRESS (ping -4 -c 1 $host != $IP_ADDRESS)"
+		fi
 	fi
 }
 
