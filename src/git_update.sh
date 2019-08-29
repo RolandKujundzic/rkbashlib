5810ca72c96a55bbaef394651560a3e093835d5d
Roland Kujundzic <roland@inkoeln.com>
2019-08-29 15:02:55 +0200

    fix repository path

--- src/git_update.sh
+++ src/git_update.sh
@@ -9,7 +9,7 @@ function _git_update {
 	_mkdir php
 	cd php
 	_git_checkout "https://github.com/RolandKujundzic/rkphplib.git" rkphplib
-	_git_checkout "rk@s1.dyn4.com:/data/git/phplib.git" phplib
+	_git_checkout "rk@s1.dyn4.com:/data/git/php/phplib.git" phplib
 	cd ..
 }
 
