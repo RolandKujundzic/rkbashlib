48bf48d5f2722795544cab827da297a2bc34328d
Roland Kujundzic <roland@kujundzic.de>
2019-09-06 04:49:21 +0200

    fix typo

--- src/syntax_check_php.sh
+++ src/syntax_check_php.sh
@@ -5,13 +5,13 @@
 #
 # @param source directory
 # @param output file
-# @global PATH_PHPLIB
+# @global PATH_RKPHPLIB
 # @require _require_global
 #------------------------------------------------------------------------------
 function _syntax_check_php {
 	local PHP_FILES=`find "$1" -type f -name '*.php'`
 
-	_require_global PATH_PHPLIB
+	_require_global PATH_RKPHPLIB
 
 	echo -e "<?php\n\ndefine('PATH_RKPHPLIB', '$PATH_RKPHPLIB');\n" > "$2"
 	echo -e "function _syntax_test(\$php_file) {\n  print \"\$php_file ... \";\n  include_once \$php_file;" >> "$2"
