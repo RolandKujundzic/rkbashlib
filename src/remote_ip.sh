2828162dddf20df28574bf91bb5ff1d5d32ef0a4
Roland Kujundzic <roland@inkoeln.com>
2019-09-07 23:51:39 +0200

    use dyn4.de/ip.php

--- src/remote_ip.sh
+++ src/remote_ip.sh
@@ -1,46 +1,17 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Export remote ip adress. Shell browser lynx is required.
+# Export remote ip adress REMOTE_IP and REMOTE_IP6.
 #
-# @export REMOTE_IP REMOTE_IP_MSG
-# @global REMOTE_IP_KEYSTROKE lynx keystroke file for IP_URL query
-# @global REMOTE_IP_URL URL which will print your _SERVER[REMOTE_ADDR]
-# @require _abort
+# @export REMOTE_IP REMOTE_IP6
+# @require _abort _require_program
 #------------------------------------------------------------------------------
 function _remote_ip {
-	local HAS_LYNX=`which lynx`
-	if test -z "$HAS_LYNX"; then
-		_abort "lynx is not installed"
-	fi
+	_require_program curl
 
-	if test -z "$REMOTE_IP_KEYSTROKE"; then
-		_abort "missing REMOTE_IP_KEYSTROKE"
-	fi
+	local IP4_IP6=`curl -sSL https://dyn4.de/ip.php`
 
-	if test -z "$REMOTE_IP_URL"; then
-		_abort "missing REMOTE_IP_URL"
-	fi
-
-	local RIP_DIR=`dirname "$REMOTE_IP_KEYSTROKE"`
-
-	if ! test -d "$RIP_DIR"; then
-	       	mkdir -p "$RIP_DIR" || _abort "failed to create $RIP_DIR directory"
-	fi
-
-	if ! test -s "$REMOTE_IP_KEYSTROKE"; then
-		echo -e "# $REMOTE_IP_URL\nkey q\nkey y" > "$REMOTE_IP_KEYSTROKE"
-	fi
-
-	REMOTE_IP_MSG=
-	REMOTE_IP=
-
-	if test -s "$REMOTE_IP_KEYSTROKE"; then
-		REMOTE_IP=`lynx -cmd_script="$REMOTE_IP_KEYSTROKE" -dump "$REMOTE_IP_URL" | xargs`
-
-		if ! test -z "$REMOTE_IP"; then
-			REMOTE_IP_MSG="- ip $REMOTE_IP"
-		fi
-	fi
+	REMOTE_IP=`echo "$IP4_IP6" | awk '{print $1}'`
+	REMOTE_IP6=`echo "$IP4_IP6" | awk '{print $2}'`
 }
 
