e4e52e081f5eecc0000dbcd6f82640ba3c05d901
Roland Kujundzic <roland@inkoeln.com>
2019-09-08 05:11:45 +0200

    check admin access too

--- src/mysql_conn.sh
+++ src/mysql_conn.sh
@@ -1,21 +1,65 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Export MYSQL_CONN connection string.
+# Export MYSQL_CONN or MYSQL (if parameter is set) connection string.
 # If MYSQL_CONN is empty but DB_NAME and DB_PASS exist use these.
+# MYSQL_CONN is "mysql -h DBHOST -u DBUSER -pDBPASS DBNAME".
+# MYSQL is "mysql -u root".
 #
-# @global MYSQL_CONN mysql connection string "-h DBHOST -u DBUSER -pDBPASS DBNAME"
 # @abort
+# @global MYSQL_CONN 
 # @require _abort
+# @param require root access
 #------------------------------------------------------------------------------
 function _mysql_conn {
 
-	if test -z "$MYSQL_CONN"; then
+	if test -z "$1" && test -z "$MYSQL_CONN"; then
 		if ! test -z "$DB_NAME" && ! test -z "$DB_PASS"; then
 			MYSQL_CONN="-h localhost -u $DB_NAME -p$DB_PASS $DB_NAME"
 		else
-			_abort "mysql connection string MYSQL_CONN is empty"
+			_abort "mysql connection string MYSQL_CONN is empty (DB_NAME=$DB_NAME)"
 		fi
 	fi
+
+	local TRY_MYSQL=
+
+	if test -z "$1"; then
+    TRY_MYSQL=`echo "USE $DB_NAME" | $MYSQL_CONN 2>&1 | grep 'ERROR 1045'`
+
+		if test -z "$TRY_MYSQL"; then
+			# MYSQL_CONN works
+			return
+		else
+			_abort "mysql connection for $DB_NAME string is invalid: $MYSQL_CONN"
+		fi
+	fi
+
+	if test -z "$MYSQL"; then
+		if ! test -z "$MYSQL_CONN"; then
+			MYSQL="$MYSQL_CONN"
+		elif ! test -z "$DB_NAME" && ! test -z "$DB_PASS"; then
+			MYSQL="-h localhost -u $DB_NAME -p$DB_PASS $DB_NAME"
+		fi
+	fi
+
+	if ! test -z "$MYSQL"; then
+    TRY_MYSQL=`echo "USE mysql" | $MYSQL 2>&1 | grep 'ERROR 1045'`
+    if ! test -z "$TRY_MYSQL" && test "$MYSQL" != "mysql -u root"; then
+      MYSQL=
+    fi
+  fi
+
+  if test -z "$MYSQL"; then
+    if test "$UID" = "0"; then
+      MYSQL="mysql -u root"
+    else
+      _abort "you must be root to run [mysql -u root]"
+    fi
+  fi
+
+  TRY_MYSQL=`echo "USE mysql" | $MYSQL 2>&1 | grep 'ERROR 1045'`
+  if ! test -z "$TRY_MYSQL" && test "$MYSQL" != "mysql -u root"; then
+    echo "admin access to mysql database failed: $MYSQL"
+  fi
 }
 
