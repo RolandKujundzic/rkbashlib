2439f83c41ecb57e385b464a5eb6d3486d7cb8ea
Roland Kujundzic <roland@inkoeln.com>
2019-09-19 17:17:21 +0200

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -48,7 +48,7 @@ function _abort {
 # Create apigen documentation for php project in docs/apigen.
 #
 # @param source directory (optional, default = src)
-# @param doc directory (optional, default = docs/api)
+# @param doc directory (optional, default = docs/apigen)
 # @require _abort _require_program _require_dir _mkdir _cd _composer_json _confirm _rm
 #------------------------------------------------------------------------------
 function _apigen_doc {
@@ -2655,6 +2655,60 @@ function _patch {
   done
 }
 
+
+#------------------------------------------------------------------------------
+# Create phpdocumentor documentation for php project in docs/phpdocumentor.
+#
+# @param source directory (optional, default = src)
+# @param doc directory (optional, default = docs/phpdocumentor)
+# @require _abort _require_program _require_dir _mkdir _cd _composer_json _confirm _rm
+#------------------------------------------------------------------------------
+function _phpdocumentor {
+  local DOC_DIR=./docs/phpdocumentor
+	local PRJ="docs/.phpdocumentor"
+	local BIN="$PRJ/vendor/phpdocumentor/phpdocumentor/bin/phpdoc"
+	local SRC_DIR=./src
+
+	_mkdir "$DOC_DIR"
+	_mkdir "$PRJ"
+	_require_program composer
+
+	local CURR="$PWD"
+
+	if ! test -f "$PRJ/composer.json"; then
+		_cd "$PRJ"
+		_composer_json "rklib/rkphplib_doc_phpdocumentor"
+		composer require "phpdocumentor/phpdocumentor:dev-master"
+		_cd "$CURR"
+	fi
+
+	if ! test -s "$BIN"; then
+		_cd "$PRJ"
+		composer update
+		_cd "$CURR"
+	fi
+
+	if ! test -z "$1"; then
+		SRC_DIR="$1"
+	fi
+
+	if ! test -z "$2"; then
+		DOC_DIR="$2"
+	fi
+
+	_require_dir "$SRC_DIR"
+
+	if test -d "$DOC_DIR"; then
+		_confirm "Remove existing documentation directory [$DOC_DIR] ?" 1
+		if test "$CONFIRM" = "y"; then
+			_rm "$DOC_DIR"
+		fi
+	fi
+
+	echo "Create phpdocumentor documentation"
+	echo "$BIN run -d '$SRC_DIR' -t '$DOC_DIR'"
+	$BIN run -d "$SRC_DIR" -t "$DOC_DIR"
+}
 
 #------------------------------------------------------------------------------
 # Export PHP_VERSION=MAJOR.MINOR
