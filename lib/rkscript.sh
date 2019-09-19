002d625b00dd280aa015fd9adfa9d0b01f6885a3
Roland Kujundzic <roland@inkoeln.com>
2019-09-19 14:03:21 +0200

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -45,20 +45,37 @@ function _abort {
 
 
 #------------------------------------------------------------------------------
-# Create apigen documentation for php project.
+# Create apigen documentation for php project in docs/apigen.
 #
 # @param source directory (optional, default = src)
 # @param doc directory (optional, default = docs/api)
-# @require _composer _abort _confirm _rm
+# @require _abort _require_program _require_dir _mkdir _cd _composer_json _confirm _rm
 #------------------------------------------------------------------------------
 function _apigen_doc {
+  local DOC_DIR=./docs/apigen
+	local PRJ="docs/.apigen"
+	local BIN="$PRJ/vendor/apigen/apigen/bin/apigen"
+	local SRC_DIR=./src
 
-	if ! test -d vender/apigen/apigen; then
-		_composer init
+	_mkdir "$DOC_DIR"
+	_mkdir "$PRJ"
+	_require_program composer
+
+	local CURR="$PWD"
+
+	if ! test -f "$PRJ/composer.json"; then
+		_cd "$PRJ"
+		_composer_json "rklib/rkphplib_doc_apigen"
+		composer require "apigen/apigen:dev-master"
+		composer require "roave/better-reflection:dev-master#c87d856"
+		_cd "$CURR"
 	fi
 
-	local SRC_DIR=./src
-	local DOC_DIR=./docs/api
+	if ! test -s "$BIN"; then
+		_cd "$PRJ"
+		composer update
+		_cd "$CURR"
+	fi
 
 	if ! test -z "$1"; then
 		SRC_DIR="$1"
@@ -68,18 +85,18 @@ function _apigen_doc {
 		DOC_DIR="$2"
 	fi
 
-	if ! test -d "$SRC_DIR"; then
-		_abort "no such directory [$SRC_DIR]"
-	fi
+	_require_dir "$SRC_DIR"
 
 	if test -d "$DOC_DIR"; then
-		_confirm "Remove existing documentation directory [$DOC_DIR] ?"
+		_confirm "Remove existing documentation directory [$DOC_DIR] ?" 1
 		if test "$CONFIRM" = "y"; then
 			_rm "$DOC_DIR"
 		fi
 	fi
 
-	vendor/apigen/apigen/bin/apigen generate -s "$SRC_DIR" -d "$DOC_DIR"
+	echo "Create apigen documentation"
+	echo "$BIN generate '$SRC_DIR' --destination '$DOC_DIR'"
+	$BIN generate "$SRC_DIR" --destination "$DOC_DIR"
 }
 
 
