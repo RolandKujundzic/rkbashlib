3d6e1d009086dad7972d836d75dc719b8bba3526
Roland Kujundzic <roland@inkoeln.com>
2019-09-23 20:21:44 +0200

    fixed _require_global ARRAY

--- src/require_global.sh
+++ src/require_global.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Abort if global variable is empty.
+# Abort if global variable is empty. With bash version >= 4.4 check works even
+# for arrays.
 #
 # @param variable name (e.g. "GLOBAL" or "GLOB1 GLOB2 ...")
 # @require _abort
 #------------------------------------------------------------------------------
 function _require_global {
+	local BASH_VERSION=`bash --version | grep -E '.+bash.+Version [0-9\.]+' | sed -E 's/^.+Version ([0-9]+)\.([0-9]+)\..+$/\1.\2/'`
+
 	local a=; for a in $1; do
-		if test -z "${!a}" && test -z "${#a[@]}"; then
-			_abort "No such global variable $a"
+		if (( $(echo "$BASH_VERSION >= 4.5" | bc -l) )); then
+			typeset -n ARR=$a
+
+			if test -z "$ARR" && test -z "${ARR[@]:1:1}"; then
+				echo "no such global variable $a"
+			fi
+		elif test -z "${!a}"; then
+			echo "no such global variable $a"
 		fi
 	done
 }
