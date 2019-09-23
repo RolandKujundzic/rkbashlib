57d479d8c3c79a406759abed5e4b57e24a42a83f
Roland Kujundzic <roland@inkoeln.com>
2019-09-23 19:31:00 +0200

    check for array too

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -2912,7 +2912,7 @@ function _require_file {
 #------------------------------------------------------------------------------
 function _require_global {
 	local a=; for a in $1; do
-		if test -z "${!a}"; then
+		if test -z "${!a}" && test -z "${#a[@]}"; then
 			_abort "No such global variable $a"
 		fi
 	done
--- src/require_global.sh
+++ src/require_global.sh
@@ -8,7 +8,7 @@
 #------------------------------------------------------------------------------
 function _require_global {
 	local a=; for a in $1; do
-		if test -z "${!a}"; then
+		if test -z "${!a}" && test -z "${#a[@]}"; then
 			_abort "No such global variable $a"
 		fi
 	done
