b0d7b92d5a7c824a2236c0291d3059be119504bc
Roland Kujundzic <roland@inkoeln.com>
2019-09-28 16:45:40 +0200

    fixed stat bug

--- src/chown.sh
+++ src/chown.sh
@@ -21,15 +21,15 @@ function _chown {
 		_abort "owner [$2] or group [$3] is empty"
 	fi
 
-	local CURR_OWNER=$(stat -c '%U' "$2")
-	local CURR_GROUP=$(stat -c '%G' "$3")
+	local CURR_OWNER=$(stat -c '%U' "$1")
+	local CURR_GROUP=$(stat -c '%G' "$1")
 
 	if test -z "$CURR_OWNER" || test -z "$CURR_GROUP"
 	then
 		_abort "stat owner [$CURR_OWNER] or group [$CURR_GROUP] of [$1] failed"
 	fi
 
-	if test "$CURR_OWNER" != "$2" || test "$CURR_GROUP" != 3
+	if test "$CURR_OWNER" != "$2" || test "$CURR_GROUP" != "$3"
 	then
 		echo "sudo chown -R '$2.$3' '$1'"
 		echo "sudo might ask for root password"
