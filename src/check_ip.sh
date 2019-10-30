88a637ce3dcf3743f662abe925e29741433efae3
Roland Kujundzic <roland@inkoeln.com>
2019-10-30 19:48:12 +0100

    fix _is_ip4 bug

--- src/check_ip.sh
+++ src/check_ip.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Abort if ip_address is ip4 and points to IP_ADDRESS.
+# Abort if ip_address of domain does not point to IP_ADDRESS.
 # Call _ip_address first.
 #
 # @global IP_ADDRESS
-# @param ip_address
+# @param domain
 # @require _abort _require_program _is_ip4
 #------------------------------------------------------------------------------
 function _check_ip {
 	_require_program ping
-
-	_is_ip4 "$1"
 
 	local IP_OK=`ping -4 -c 1 "$1" 2> /dev/null | grep "$IP_ADDRESS"`
 	if test -z "$IP_OK"; then
