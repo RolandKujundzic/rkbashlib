c5509d2cddbdc12fd7606fc7b5f07cbd1167a467
Roland Kujundzic <roland@inkoeln.com>
2019-10-30 23:24:55 +0100

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -877,15 +877,15 @@ function _cordova_create {
 function _cp {
 	local TARGET=`dirname "$2"`
 
-  local CURR_LOG_NO_ECHO=$LOG_NO_ECHO
-  LOG_NO_ECHO=1
+	local CURR_LOG_NO_ECHO=$LOG_NO_ECHO
+	LOG_NO_ECHO=1
 
 	if ! test -d "$TARGET"; then
 		_abort "no such directory [$TARGET]"
 	fi
 
 	if test "$3" = "md5" && test -f "$1" && test -f "$2"; then
-	  local MD1=`_md5 "$1"`
+		local MD1=`_md5 "$1"`
 		local MD2=`_md5 "$2"`
 
 		if test "$MD1" = "$MD2"; then
@@ -896,10 +896,10 @@ function _cp {
 		fi
 
 		return
-  fi
+	fi
 
-  if test -f "$1"; then
-    echo "Copy file $1 to $2"
+	if test -f "$1"; then
+		echo "Copy file $1 to $2"
 		_sudo "cp '$1' '$2'" 1
 	elif test -d "$1"; then
 		if test -d "$2"; then
@@ -912,7 +912,7 @@ function _cp {
 		fi
 	else
 		_abort "No such file or directory [$1]"
-  fi
+	fi
 
 	LOG_NO_ECHO=$CURR_LOG_NO_ECHO
 }
