39cdf5894a5d9f07a3abf0b7d00b94d6629be844
Roland Kujundzic <roland@inkoeln.com>
2019-11-09 17:21:55 +0100

    initial

---
+++ src/mount.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Mount $1 (e.g. /dev/sdb2) to $2 (e.g. /mnt)
+#
+# @param device
+# @param directory (mount point)
+# @require _abort _confirm
+#------------------------------------------------------------------------------
+function _mount {
+	local HAS_FS=`file -sL $1 | grep ' filesystem'`
+	test -z "$HAS_FS" && _abort "no filesystem on $1"
+
+	local HAS_MOUNT=`mount | grep -E "^$1 on $2"`
+
+	if test -z "$HAS_MOUNT"; then
+		HAS_MOUNT=`mount | grep '$1 on '`
+		if ! test -z "$HAS_MOUNT"; then
+			_confirm "umount $1 (and re-mount as $2)" 1
+			test "$CONFIRM" = "y" || _abort "user abort"
+			umount /dev/sdb2 || _abort "umount /dev/sdb2"
+		fi
+
+		_confirm "Mount $1 as $2"
+		if test "$CONFIRM" = "y"; then
+			mount $1 "$2" || _abort "mount $1 '$2'"
+		fi
+
+		HAS_MOUNT=`mount | grep '$1 on $2'`
+		test -z "$HAS_MOUNT" && _abort "failed to mount $1 as $2"
+	else
+		echo "$1 is already mounted as $2"
+	fi
+}
+
