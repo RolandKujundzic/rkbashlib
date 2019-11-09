d267173ffcdd617b8d0a913432c6df0a29e709c9
Roland Kujundzic <roland@inkoeln.com>
2019-11-09 14:03:27 +0100

    initial

---
+++ src/find.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Save found filesystem entries into FOUND.
+#
+# @param any paramter useable with find command
+# @export FOUND Path Array
+# @required _require_program
+#------------------------------------------------------------------------------
+function _find {
+	FOUND=()
+	local a=
+
+	_require_program find
+
+	while read a; do
+		FOUND+=("$a")
+	done <<< `find $@ 2>/dev/null`
+}
+
