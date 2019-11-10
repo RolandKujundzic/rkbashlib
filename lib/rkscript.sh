fd39669bb7b5cfc75fe9c11642bcc727bcd32b1f
Roland Kujundzic <roland@inkoeln.com>
2019-11-10 11:29:50 +0100

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -2676,6 +2676,33 @@ function _npm_module {
 }
 
 #------------------------------------------------------------------------------
+# Backup $1 as $1.orig (if not already done).
+#
+# @param path
+# @require _cp _abort
+#------------------------------------------------------------------------------
+function _orig {
+	if test -f "$1"; then
+		if test -f "$1.orig"; then
+			echo "Backup $1 as $1.orig"
+			_cp "$1" "$1.orig"
+		else
+			echo "Backup $1.orig already exists"
+		fi
+	elif test -d "$1"; then
+		if test -d "$1.orig"; then
+			echo "Backup $1 as $1.orig"
+			_cp "$1" "$1.orig"
+		else
+			echo "Backup $1.orig already exists"
+		fi
+	else
+		_abort "no such file or directory: $1"
+	fi
+}
+
+
+#------------------------------------------------------------------------------
 # Return linux, macos, cygwin.
 #
 # @print string (abort if set and os_type != $1)
