cb86dabf1038e20acc86f09085617123c3272ab5
Roland Kujundzic <roland@inkoeln.com>
2019-11-10 11:11:33 +0100

    fix ''

--- src/mount.sh
+++ src/mount.sh
@@ -9,12 +9,17 @@
 #------------------------------------------------------------------------------
 function _mount {
 	local HAS_FS=`file -sL $1 | grep ' filesystem'`
-	test -z "$HAS_FS" && _abort "no filesystem on $1"
+	if test -z "$HAS_FS"; then
+		# check if fat32 boot
+		HAS_FS=`file -sL $1 | grep 'MBR boot sector'`
+
+		test -z "$HAS_FS" && _abort "no filesystem on $1"
+	fi
 
 	local HAS_MOUNT=`mount | grep -E "^$1 on $2"`
 
 	if test -z "$HAS_MOUNT"; then
-		HAS_MOUNT=`mount | grep '$1 on '`
+		HAS_MOUNT=`mount | grep -E "^$1 on "`
 		if ! test -z "$HAS_MOUNT"; then
 			_confirm "umount $1 (and re-mount as $2)" 1
 			test "$CONFIRM" = "y" || _abort "user abort"
@@ -26,7 +31,7 @@ function _mount {
 			mount $1 "$2" || _abort "mount $1 '$2'"
 		fi
 
-		HAS_MOUNT=`mount | grep '$1 on $2'`
+		HAS_MOUNT=`mount | grep -E "^$1 on $2"`
 		test -z "$HAS_MOUNT" && _abort "failed to mount $1 as $2"
 	else
 		echo "$1 is already mounted as $2"
