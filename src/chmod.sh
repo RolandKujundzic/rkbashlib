b9c2b649a06c6c255c8b56619a711466bc795b2e
Roland Kujundzic <roland@inkoeln.com>
2019-11-14 11:23:16 +0100

    update

--- src/chmod.sh
+++ src/chmod.sh
@@ -2,43 +2,42 @@
 
 #------------------------------------------------------------------------------
 # Change mode of path $2 to $1. If chmod failed try sudo.
+# Use _find first to chmod all FOUND entries.
 #
 # @param file mode (octal)
-# @param file path
+# @param file path (if path is empty use $FOUND)
 # @require _abort _sudo
 #------------------------------------------------------------------------------
 function _chmod {
+	test -z "$1" && _abort "empty privileges parameter"
 	test -z "$2" && _abort "empty path"
 
-	local ENTRY=()
-	local a=; local i=;
-
-	if ! test -f "$2" && ! test -d "$2"; then
-		while read a; do
-			ENTRY+=("$a")
-		done <<< `find "$2" 2>/dev/null`
-	else
-		ENTRY+=("$2")
-	fi
-
-	test ${#ENTRY[@]} -lt 1 && _abort "invalid path [$2]"
-
-	if test -z "$1"; then
-		_abort "empty privileges parameter"
-	fi
-
 	local tmp=`echo "$1" | sed -e 's/[012345678]*//'`
+	test -z "$tmp" || _abort "invalid octal privileges '$1'"
 
-	if ! test -z "$tmp"; then
-		_abort "invalid octal privileges '$1'"
-	fi
+	local a=; local i=; local PRIV=;
 
-	for ((i = 0; i < ${#ENTRY[@]}; i++)); do
-		local PRIV=`stat -c "%a" "${ENTRY[$i]}"`
+	if test -z "$2"; then
+		for ((i = 0; i < ${#FOUND[@]}; i++)); do
+			PRIV=
+
+			if test -f "${FOUND[$i]}" || test -d "${FOUND[$i]}"; then
+				PRIV=`stat -c "%a" "${FOUND[$i]}"`
+			fi
+
+			if test "$1" != "$PRIV" && test "$1" != "0$PRIV"; then
+				_sudo "chmod -R $1 '${FOUND[$i]}'" 1
+			fi
+		done
+	elif test -f "$2"; then
+		PRIV=`stat -c "%a" "$2"`
 
 		if test "$1" != "$PRIV" && test "$1" != "0$PRIV"; then
-			_sudo "chmod -R $1 '${ENTRY[$i]}'" 1
+			_sudo "chmod -R $1 '$2'" 1
 		fi
-	done
+	else
+		# no stat compare because subdir entry may have changed
+		_sudo "chmod -R $1 '$2'" 1
+	fi
 }
 
