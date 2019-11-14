6c593ff89c21612ccd760f79c7758f67815ccc30
Roland Kujundzic <roland@inkoeln.com>
2019-11-14 13:52:57 +0100

    update

--- test/chown.sh
+++ test/chown.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 . ../lib/rkscript.sh || exit 1
+. testHelper.sh || exit 1
 
+APP="$0"
+
+_rm out
 _mkdir out
 
 echo "a" > out/a.txt
 echo "b c" > out/b\ c.txt
+
+_ls_out
+_chown out rk www-data 
+_ls_out
+
+_compare_ok
 
