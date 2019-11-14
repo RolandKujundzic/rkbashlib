7827b1b4d13311cb58b82df52494bcd838864d2b
Roland Kujundzic <roland@inkoeln.com>
2019-11-14 13:53:31 +0100

    initial

---
+++ test/testHelper.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+declare -A TEST_OUT
+
+#------------------------------------------------------------------------------
+# Add [find out -type f -printf "%u %g '%f'\n"] output to TEST_OUT
+#------------------------------------------------------------------------------
+function _ls_out {
+	_require_dir out
+	TEST_OUT[${#TEST_OUT[@]}]=`find out -type f -printf "%u %g '%f'\n"`
+}
+
+
+#------------------------------------------------------------------------------
+# Compare TEST_OUT with ok/$APP.ok.N.txt
+# @global APP
+#------------------------------------------------------------------------------
+function _compare_ok {
+	_require_global APP
+	echo -e "\n$APP: ${#TEST_OUT[@]} tests\n------------------------------"
+
+	local a=; local i=; local n=; local ok=; local out=; local err=0;
+
+	for ((i = 0; i < ${#TEST_OUT[@]}; i++)); do
+		out="${TEST_OUT[$i]}"
+		n=$((i + 1))
+		_require_file "ok/$APP.ok.$n.txt"
+		ok=`cat "ok/$APP.ok.$n.txt"`
+
+		if test "$ok" != "$out"; then
+			echo "$out" > "ok/$APP.out.$n.txt"
+			echo "  $n / ${#TEST_OUT[@]} ... ERROR (ok/$APP.out.$n.txt != ok/$APP.ok.$n.txt)"
+			err=$((err + 1))
+		else
+			echo "  $n / ${#TEST_OUT[@]} ... OK"
+		fi
+	done
+
+	if test $err -gt 0; then
+		_abort "$APP test failed - fix $err error(s)"
+	else
+		echo -e "------------------------------\n$n test(s) succeded\n"
+	fi
+}
+
