15b0e2d11a7aabeb9ef7ef870d36c18f3c46d6fb
Roland Kujundzic <roland@inkoeln.com>
2019-11-14 13:53:16 +0100

    update

--- src/log.sh
+++ src/log.sh
@@ -26,10 +26,23 @@ function _log {
 	LOG_FILE[$2]="$RKSCRIPT_DIR/$2/${LOG_COUNT[$2]}.nfo"
 	LOG_CMD[$2]=">> '${LOG_FILE[$2]}' 2>&1"
 
-	test -d "$RKSCRIPT_DIR/$2" || ( mkdir -p "$RKSCRIPT_DIR/$2" && chmod 777 "$RKSCRIPT_DIR/$2" )
+	if ! test -d "$RKSCRIPT_DIR/$2"; then
+		mkdir -p "$RKSCRIPT_DIR/$2"
+		if ! test -z "$SUDO_USER"; then
+			chown -R $SUDO_USER.$SUDO_USER "$RKSCRIPT_DIR" || _abort "chown -R $SUDO_USER.$SUDO_USER '$RKSCRIPT_DIR'"
+		elif test "$UID" = "0"; then
+			chmod -R 777 "$RKSCRIPT_DIR" || _abort "chmod -R 777 '$RKSCRIPT_DIR'"
+		fi
+	fi
 
 	local NOW=`date +'%d.%m.%Y %H:%M:%S'`
 	echo -e "# _$2: $NOW\n# $PWD\n# $1 ${LOG_CMD[$2]}\n" > "${LOG_FILE[$2]}"
+
+	if ! test -z "$SUDO_USER"; then
+		chown $SUDO_USER.$SUDO_USER "${LOG_FILE[$2]}" || _abort "chown $SUDO_USER.$SUDO_USER '${LOG_FILE[$2]}'"
+	elif test "$UID" = "0"; then
+		chmod 666 "${LOG_FILE[$2]}" || _abort "chmod 666 '${LOG_FILE[$2]}'"
+	fi
 
 	test -z "$LOG_NO_ECHO" && echo " ${LOG_CMD[$2]}"
 }
