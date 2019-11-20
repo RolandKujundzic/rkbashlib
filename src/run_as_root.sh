54801a8391321aaf1bc30438e71313b0c7865789
Roland Kujundzic <roland@inkoeln.com>
2019-11-20 12:44:01 +0100

    allow sudo

--- src/run_as_root.sh
+++ src/run_as_root.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 #------------------------------------------------------------------------------
-# Abort if user is not root.
+# Abort if user is not root. If sudo cache time is ok allow sudo with $1 = 1.
 #
+# @param try sudo
 # @require _abort
 #------------------------------------------------------------------------------
 function _run_as_root {
-	if test "$UID" != "0"; then
+	test "$UID" = "0" && return
+
+	if test -z "$1"; then
+		_abort "Please change into root and try again"
+	else
 		echo "sudo true - Please type in your password"
 		sudo true 2>/dev/null || _abort "sudo true failed - Please change into root and try again"
 	fi
