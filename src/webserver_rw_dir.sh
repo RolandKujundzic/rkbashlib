2c72ecab0802b1d1924158211faef2741e9b8b36
Roland Kujundzic <roland@inkoeln.com>
2019-11-21 16:33:25 +0100

    initial

---
+++ src/webserver_rw_dir.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------
+# Make directory $1 read|writeable for webserver.
+#
+# @param directory path
+# @require _abort _chown _chmod
+#------------------------------------------------------------------------------
+function _webserver_rw_dir {
+	test -d "$1" || _abort "no such directory $1"
+
+	local DIR_OWNER=`stat -c '%U' "$1"`
+	local SERVER_USER=
+
+	if test -s "/etc/apache2/envvars"; then
+		SERVER_USER=`cat /etc/apache2/envvars | grep -E '^export APACHE_RUN_USER=' | sed -E 's/.*APACHE_RUN_USER=//'`
+	fi
+
+	if ! test -z "$SERVER_USER" && "$SERVER_USER" = "$DIR_OWNER"; then
+		echo "directory $1 is already owned by webserver $SERVER_USER"
+		return
+	fi
+
+	_chmod 770 "$1"
+
+	local ME="$USER"
+	test -z "$SUDO_USER" || ME="$SUDO_USER"
+
+	_chown "$1" "$ME" "$SERVER_USER"
+}
+
