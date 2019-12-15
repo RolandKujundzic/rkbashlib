5cd0ee25f1baf933b536c94f41b09f0f4068bfd8
Roland Kujundzic <roland@inkoeln.com>
2019-12-15 13:11:25 +0100

    _run_as_root added

--- src/apt_install.sh
+++ src/apt_install.sh
@@ -9,12 +9,14 @@ function _apt_install {
 	local CURR_LOG_NO_ECHO=$LOG_NO_ECHO
 	LOG_NO_ECHO=1
 
+	_run_as_root 1
+
 	for a in $1
 	do
 		if test -d "$RKSCRIPT_DIR/apt/$a"; then
 			echo "already installed, skip: apt -y install $a"
 		else
-			apt -y install $a || _abort "apt -y install $a"
+			sudo apt -y install $a || _abort "apt -y install $a"
 			_log "apt -y install $a" apt/$a
 		fi
 	done
