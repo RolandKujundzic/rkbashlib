3f36eea899dce7c14b0df0f9128bc58b67272e15
Roland Kujundzic <roland@inkoeln.com>
2019-12-16 15:04:07 +0100

    update wget

--- src/wget.sh
+++ src/wget.sh
@@ -8,41 +8,36 @@
 # @require _abort _require_program _confirm
 #--
 function _wget {
-	if test -z "$1"; then
-		_abort "empty url"
-	fi
-
+	test -z "$1" && _abort "empty url"
 	_require_program wget
 
-	if ! test -z "$2" && test "$2" != "-" && test -f "$2"; then
-		_confirm "Overwrite $2" 1
+	local SAVE_AS=${2:-`basename "$1"`}
+	if test -s "$SAVE_AS"; then
+		_confirm "Overwrite $SAVE_AS" 1
 		if test "$CONFIRM" != "y"; then
-			echo "keep $2 - skip wget '$1'"
+			echo "keep $SAVE_AS - skip wget '$1'"
 			return
 		fi
 	fi
 
 	if test -z "$2"; then
 		echo "download $1"
-		wget -q "$1"
+		wget -q "$1" || _abort "wget -q '$1'"
 	elif test "$2" = "-"; then
-		wget -q -O "$2" "$1"
+		wget -q -O "$2" "$1" || _abort "wget -q -O '$2' '$1'"
+		return
 	else
 		echo "download $1 as $2"
-		wget -q -O "$2" "$1"
+		wget -q -O "$2" "$1" || _abort "wget -q -O '$2' '$1'"
 	fi
 
-  if test "$2" != "-"; then
-		if test -z "$2"; then
-			local SAVE_AS=`basename "$1"`
+	if test -z "$2"; then
+		if ! test -s "$SAVE_AS"; then
 			local NEW_FILES=`find . -amin 1 -type f`
-
-			if ! test -s "$SAVE_AS" && test -z "$NEW_FILES"; then
-				_abort "Download from $1 failed"
-			fi
-		elif ! test -s "$2"; then
-			_abort "Download of $2 from $1 failed"
+			test -z "$NEW_FILES" && _abort "Download from $1 failed"
 		fi
-  fi
+	elif ! test -s "$2"; then
+		_abort "Download of $2 from $1 failed"
+	fi
 }
 
