8581c5f25aa8c8d960a1ae4f67f63b7b34d5c611
Roland Kujundzic <roland@inkoeln.com>
2019-12-17 22:01:44 +0100

    require_program added

--- src/os_type.sh
+++ src/os_type.sh
@@ -4,9 +4,12 @@
 # Return linux, macos, cygwin.
 #
 # @print string (abort if set and os_type != $1)
+# @require _abort _require_program
 #--
 function _os_type {
 	local os=
+
+	_require_program uname
 
 	if [ "$(uname)" = "Darwin" ]; then
 		os="macos"        
