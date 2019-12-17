5fcb882ec186b6ecddaf9b64da0a9aa1ae124f40
Roland Kujundzic <roland@inkoeln.com>
2019-12-17 22:35:02 +0100

    initial

---
+++ example/variable.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+function test {
+	local L1="local variable 1"
+	local v="L2"
+	# declare -g = declare global (only if bash version >= 4.2)
+	declare "$v"="local variable 2"
+	echo "L1=$L1 L2=$L2"
+}
+
+
+#
+# M A I N
+#
+
+echo -e "\nB is variable reference (variable pointer)"
+
+A="Hello World"
+B=A
+#error: B="something else"
+
+echo "A=$A | A=${A} (different syntax) | B=$B | B=${!B} (value of B)"
+
+echo -e "\nUse parameter in variable assignment"
+
+test
+
+printf -v "TEST" '%s' "Hello"
+echo "TEST=$TEST L1=$L1 L2=$L2"
+
+declare "G"="global variable"
+eval "TEST2='eval is evil'"
+echo "G=$G TEST2=$TEST2"
+
