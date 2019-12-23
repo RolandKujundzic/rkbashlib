462d74d5c9a37c75aa1c3b918f401d947c89b410
Roland Kujundzic <roland@inkoeln.com>
2019-12-23 17:34:33 +0100

    update

--- example/boolean_operation.sh
+++ example/boolean_operation.sh
@@ -9,6 +9,19 @@ function boolean_operation {
 	echo
 }
 
+function tf {
+	local A=$1
+	local B=$2
+	local A_AND_B=
+	local A_OR_B=	
+
+	if ((A && B)); then A_AND_B=1; else A_AND_B=0; fi
+	if ((A || B)); then A_OR_B=1; else A_OR_B=0; fi
+
+	echo "A=[$A] B=[$B]: '$A' && '$B' = $((A && B)), '$A' || '$B' = $((A || B)), if (A && B) = $A_AND_B, if (A || B) = $A_OR_B"
+}
+
+
 for A in 0 1; do
 	for B in 0 1; do
 		boolean_operation "$A" "$B"
@@ -23,7 +36,7 @@ done
 
 A="x"; B="y";
 echo "Only number or empty string allowed:"
-echo "'$A' && '$B' = $((A && B))"
-echo "'$A' || '$B' = $((A || B))"
-A="1"; B="";
-echo "'$A' || '$B' = $((A || B))"
+tf "x" "y"
+tf "" 1
+tf 1 ""
+
