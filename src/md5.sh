07a248f31b64141719463fecf71ab7a31e762678
Roland Kujundzic <roland@inkoeln.com>
2019-12-23 16:42:31 +0100

    remove filename from output

--- src/md5.sh
+++ src/md5.sh
@@ -4,18 +4,16 @@
 # Print md5sum of file.
 #
 # @param file
-# @require _abort
+# @require _abort _require_program
 # @print md5sum
 #--
 function _md5 {
-	if test -z "$1" || ! test -f "$1"
-	then
+	_require_program md5sum
+	
+	if test -z "$1" || ! test -s "$1"; then
 		_abort "No such file [$1]"
 	fi
 
-	# use MD5 to drop filename
-	local MD5=$(md5sum "$1")
-
-	echo $MD5
+	md5sum "$1" | awk '{print $1}'
 }
 
