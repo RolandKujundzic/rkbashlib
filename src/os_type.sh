d1a50f46dce8261b38fb09be2db22dadbeeec531
Roland Kujundzic <roland@inkoeln.com>
2019-12-23 18:56:45 +0100

    return bool

--- src/os_type.sh
+++ src/os_type.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 #--
-# Return linux, macos, cygwin.
+# Return linux, macos, cygwin if $1 is empty. 
+# If $1 is set and != os_type abort otherwise return 0.
 #
 # @print string (abort if set and os_type != $1)
 # @require _abort _require_program
+# @print linux|macos|cygwin if $1 is empty
+# @return bool
 #--
 function _os_type {
 	local os=
@@ -21,9 +24,11 @@ function _os_type {
 		os="cygwin"
 	fi
 
-	if ! test -z "$1" && test "$1" != "$os"; then
-		_abort "$os required (this is $os)"
+	if test -z "$1"; then
+		echo $os
+	elif test "$1" != "$os"; then
+		_abort "$1 required (this is $os)"
 	fi
 
-	echo $os
+	return 0
 }
