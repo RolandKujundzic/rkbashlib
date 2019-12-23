a08da1e992a30bf18d7d4e94736eeffd19983210
Roland Kujundzic <roland@inkoeln.com>
2019-12-23 19:07:38 +0100

    update

--- src/stop_http.sh
+++ src/stop_http.sh
@@ -8,28 +8,28 @@
 # @os linux
 #--
 function _stop_http {
-  _os_type linux
+	_os_type linux
 
-  if test "$(_is_running PORT 80)" != "PORT_running"; then
-    echo "no service on port 80"
-    return
-  fi 
+	if ! _is_running PORT 80; then
+		echo "no service on port 80"
+		return
+	fi
 
-  if test "$(_is_running DOCKER_PORT_80)" = "DOCKER_PORT_80_running"; then
-    echo "ignore docker service on port 80"
-    return
-  fi
+	if _is_running DOCKER_PORT_80; then
+		echo "ignore docker service on port 80"
+		return
+	fi
 
-  if test "$(_is_running NGINX)" = "NGINX_running"; then
-    echo "stop nginx"
-    sudo service nginx stop
-    return
-  fi
+	if _is_running NGINX; then
+		echo "stop nginx"
+		sudo service nginx stop
+		return
+	fi
 
-  if test "$(_is_running APACHE2)" = "APACHE2_running"; then
-    echo "stop apache2"
-    sudo service apache2 stop
-    return
-  fi
+	if _is_running APACHE2; then
+		echo "stop apache2"
+		sudo service apache2 stop
+		return
+	fi
 }
 
