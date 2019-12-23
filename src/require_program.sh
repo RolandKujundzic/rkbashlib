e6dbee60707b5849f867f090c6b764de60d4c535
Roland Kujundzic <roland@inkoeln.com>
2019-12-23 17:35:34 +0100

    fixed boolean error

--- src/require_program.sh
+++ src/require_program.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 #--
-# Abort if program (function) does not exist
+# Abort if program (function) $1 does not exist (and $2 is not 1).
 #
 # @param program
-# @param flag (default = 0, 1: return 1 if not found)
+# @param return_bool (default = 0)
 # @require _abort
 # @return bool (if $2==1)
 #--
 function _require_program {
 	local TYPE=`type -t "$1"`
 	local ERROR=0
+  local CHECK=$2
 
 	test "$TYPE" = "function" && return $ERROR
 
 	command -v "$1" >/dev/null 2>&1 || ERROR=1
 
-	if test -z "$2" && ! test -z "$ERROR"; then
-		_abort "No such program [$1]"
-	fi
+  if ((!CHECK && ERROR)); then
+    echo "No such program [$1]" && exit 1
+  fi
 
 	return $ERROR
 }
