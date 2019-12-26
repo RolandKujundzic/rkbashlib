1fbe749bae0bda79951401ace04624b5aaa0042f
Roland Kujundzic <roland@inkoeln.com>
2019-12-26 19:36:50 +0100

    bugfix

--- src/orig.sh
+++ src/orig.sh
@@ -9,17 +9,17 @@
 function _orig {
 	if test -f "$1"; then
 		if test -f "$1.orig"; then
+			echo "Backup $1.orig already exists"
+		else
 			echo "Backup $1 as $1.orig"
 			_cp "$1" "$1.orig"
-		else
-			echo "Backup $1.orig already exists"
 		fi
 	elif test -d "$1"; then
 		if test -d "$1.orig"; then
+			echo "Backup $1.orig already exists"
+		else
 			echo "Backup $1 as $1.orig"
 			_cp "$1" "$1.orig"
-		else
-			echo "Backup $1.orig already exists"
 		fi
 	else
 		_abort "no such file or directory: $1"
