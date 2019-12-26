fdfb4bc9cf9bbd0d3989b6e2b1aca1ebb2034a8f
Roland Kujundzic <roland@inkoeln.com>
2019-12-26 22:11:56 +0100

    allow empty parameter $2

--- src/git_checkout.sh
+++ src/git_checkout.sh
@@ -10,18 +10,19 @@
 # - else: git clone rk@git.tld:/path/to/repo test
 #
 # @param git url
-# @param local directory
+# @param local directory (optional, default = basename $1 without .git)
 # @param after_checkout (e.g. "./run.sh build")
 # @global CONFIRM_CHECKOUT (if =1 use positive confirm if does not exist) GIT_PARAMETER
 # @require _abort _confirm _cd _ln
 #--
 function _git_checkout {
 	local CURR="$PWD"
+	local GIT_DIR="${2:-`basename "$1" | sed -E 's/\.git$//'`}"
 
-	if test -d "$2"; then
-		_confirm "Update $2 (git pull)?" 1
+	if test -d "$GIT_DIR"; then
+		_confirm "Update $GIT_DIR (git pull)?" 1
 	elif ! test -z "$CONFIRM_CHECKOUT"; then
-		_confirm "Checkout $1 to $2 (git clone)?" 1
+		_confirm "Checkout $1 to $GIT_DIR (git clone)?" 1
 	fi
 
 	if test "$CONFIRM" = "n"; then
@@ -29,34 +30,34 @@ function _git_checkout {
 		return
 	fi
 
-	if test -d "$2"; then
-		_cd "$2"
-		echo "git pull $2"
+	if test -d "$GIT_DIR"; then
+		_cd "$GIT_DIR"
+		echo "git pull $GIT_DIR"
 		git pull
 		test -s .gitmodules && git submodule update --init --recursive --remote
 		test -s .gitmodules && git submodule foreach "(git checkout master; git pull)"
 		_cd "$CURR"
-	elif test -d "../../$2" && ! test -L "../../$2"; then
-		_ln "../../$2" "$2"
-		_git_checkout "$1" "$2"
+	elif test -d "../../$GIT_DIR/.git" && ! test -L "../../$GIT_DIR"; then
+		_ln "../../$GIT_DIR" "$GIT_DIR"
+		_git_checkout "$1" "$GIT_DIR"
 	else
-		echo -e "git clone $GIT_PARAMETER '$1' '$2'\nEnter password if necessary"
-		git clone $GIT_PARAMETER "$1" "$2"
+		echo -e "git clone $GIT_PARAMETER '$1' '$GIT_DIR'\nEnter password if necessary"
+		git clone $GIT_PARAMETER "$1" "$GIT_DIR"
 
-		if ! test -d "$2/.git"; then
-			_abort "git clone failed - no $2/.git directory"
+		if ! test -d "$GIT_DIR/.git"; then
+			_abort "git clone failed - no $GIT_DIR/.git directory"
 		fi
 
-		if test -s "$2/.gitmodules"; then
-			_cd "$2"
+		if test -s "$GIT_DIR/.gitmodules"; then
+			_cd "$GIT_DIR"
 			test -s .gitmodules && git submodule update --init --recursive --remote
 			test -s .gitmodules && git submodule foreach "(git checkout master; git pull)"
 			_cd ..
 		fi
 
 		if ! test -z "$3"; then
-			_cd "$2"
-			echo "run [$3] in $2"
+			_cd "$GIT_DIR"
+			echo "run [$3] in $GIT_DIR"
 			$3
 			_cd ..
 		fi
