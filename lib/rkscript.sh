bd7e98642f4e6b0789486f032666828403a307fd
Roland Kujundzic <roland@kujundzic.de>
2019-12-28 07:31:46 +0100

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -2107,20 +2107,25 @@ function _mb_check {
 
 
 #--
-# Print md5sum of file.
+# Print md5sum of file (text if $2=1).
 #
 # @param file
+# @param bool (optional: 1 = threat $1 as string)
 # @require _abort _require_program
 # @print md5sum
 #--
 function _md5 {
 	_require_program md5sum
 	
-	if test -z "$1" || ! test -s "$1"; then
+	if test -z "$1"; then
+		_abort "Empty parameter"
+	elif test -f "$1"; then
+		md5sum "$1" | awk '{print $1}'
+	elif test "$2" = "1"; then
+		echo -n "$1" | md5sum | awk '{print $1}'
+	else
 		_abort "No such file [$1]"
 	fi
-
-	md5sum "$1" | awk '{print $1}'
 }
 
 
