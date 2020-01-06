fa7982245f513ff73bfa457948e94f86f17e62f7
Roland Kujundzic <roland@inkoeln.com>
2020-01-06 09:58:04 +0100

    reset ARG if ARG[0] is set

--- src/parse_arg.sh
+++ src/parse_arg.sh
@@ -5,13 +5,14 @@ declare -A ARG
 #--
 # Set ARG[name]=value if --name=value or name=value.
 # If --name set ARG[name]=1. Set ARG[0] ... ARG[n].
+# Reset ARG if ARG[0] is set.
 #
 # @parameter any
 # @global ARG (hash)
 #--
 function _parse_arg {
 	local key=; local i=; local v=;
-	ARG=()
+	test -z "${ARG[0]}" || ARG=()
 
 	for (( i = 0; i <= $#; i++ )); do
 		ARG[$i]="${!i}"
