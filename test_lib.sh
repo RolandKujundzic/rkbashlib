685f74f847f87deed94f02b4a78ee5fee6ffe5f2
Roland Kujundzic <roland@kujundzic.de>
2020-01-07 10:28:06 +0100

    update

--- test_lib.sh
+++ test_lib.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-. /usr/local/lib/rkscript.sh || { echo "ERROR: . /usr/local/lib/rkscript.sh"; exit 1; }
+#. /usr/local/lib/rkscript.sh || { echo "ERROR: . /usr/local/lib/rkscript.sh"; exit 1; }
+. lib/rkscript.sh || { echo "ERROR: . /usr/local/lib/rkscript.sh"; exit 1; }
 
 
 #--
