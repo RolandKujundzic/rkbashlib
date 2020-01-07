e0ed9f7fd138003fc4f402f030b084a41ed1d675
Roland Kujundzic <roland@kujundzic.de>
2020-01-07 07:11:16 +0100

    initial

---
+++ src/crontab.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+	
+#--
+# Install user ($1) crontab ($2 $3).
+#
+# @param string user
+# @param string repeat-time
+# @param string command
+# @require _abort
+#--
+function _crontab {
+	local CRONTAB_DIR="/var/spool/cron/crontabs"
+	log "install $1 crontab: $2 $3"
+	_mkdir "$CRONTAB_DIR" >/dev/null
+
+	grep "$3" "$CRONTAB_DIR/$1" && { echo "already installed - skip"; return; }
+	echo "$2 $3" >>"$CRONTAB_DIR/$1" || _abort "failed to create $1 cron"
+}
+
