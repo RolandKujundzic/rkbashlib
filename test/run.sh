c62e5365425c4baa823a06c4d3789a9f1d1b789d
Roland Kujundzic <roland@kujundzic.de>
2020-01-07 10:19:40 +0100

    update

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-./chown.sh
-./chmod.sh
+. ../lib/rkscript.sh || { echo "failed to load ../lib/rkscript.sh"; exit 1; }
+. testHelper.sh || { echo "failed to load testHelper.sh"; exit 1; }
+
+APP="$0"
+
+TEST_CALL[total]=`ls inc/*.sh | wc -l`
+
+test_start "call/*sh: run ${TEST_CALL[total]} test(s)"
+for a in call/*.sh; do
+	test_call `basename $a`
+done
+
+test_done ${TEST_CALL[ok]} ${TEST_CALL[err]} 
+
+for a in inc/*.sh; do
+	test_start
+	. $a
+	ls_out
+	compare_ok `basename $a`
+done
--- test/testHelper.sh
+++ test/testHelper.sh
@@ -5,23 +5,79 @@ declare -A TEST_OUT
 test -z "$TEST_HELPER_SH" || return
 TEST_HELPER_SH=1
 
+declare -A TEST_CALL=( [total]=0 [ok]=0 [err]=0 )
+
+
+#------------------------------------------------------------------------------
+# Run inc/$1 and compare output with ok/$1.txt.
+#
+# @param test script
+#------------------------------------------------------------------------------
+function test_call {
+	_require_dir out
+	local base=`echo "$1" | sed -E 's/\.sh$//'`
+	test -f "ok/$1.txt" || _abort "missing ok/$1.txt"
+
+	echo -n "_$base ... "
+	. call/$1 >"out/$1.txt" 2>"out/$1.err"
+
+	local diff=`diff -u "out/$1.txt" "ok/$1.txt"`	
+	if test -z "$diff" && ! test -s "out/$1.err"; then
+		TEST_CALL[ok]=$((TEST_CALL[ok] + 1))
+		echo "ok"
+	else
+		TEST_CALL[err]=$((TEST_CALL[err] + 1))
+		echo "error"
+	fi
+}
+
 
 #------------------------------------------------------------------------------
 # Add [find out -type f -printf "%u %g '%f'\n"] output to TEST_OUT
 #------------------------------------------------------------------------------
-function _ls_out {
+function ls_out {
 	_require_dir out
 	TEST_OUT[${#TEST_OUT[@]}]=`find out -type f -printf "%u %g %M '%p'\n"`
 }
 
 
 #------------------------------------------------------------------------------
-# Compare TEST_OUT with ok/$APP.ok.N.txt
-# @global APP
+# Print test header
+#
+# @param string
 #------------------------------------------------------------------------------
-function _compare_ok {
-	_require_global APP
-	echo -e "\n$APP: ${#TEST_OUT[@]} tests\n------------------------------"
+function test_start {
+	_rm out >/dev/null
+	_mkdir out >/dev/null
+
+	TEST_OUT=()
+
+	test -z "$1" || echo -e "\n$1\n------------------------------"
+}
+
+
+#------------------------------------------------------------------------------
+# Print test footer
+#
+# @param int #success
+# @param int #error
+#------------------------------------------------------------------------------
+function test_done {
+	if test "$2" -gt 0; then
+		_abort "$2 test(s) failed"
+	else
+		echo -e "------------------------------\n$1 test(s) succeded\n"
+	fi
+}
+
+
+#------------------------------------------------------------------------------
+# Compare TEST_OUT with ok/$APP.ok.N.txt
+# @param script name
+#------------------------------------------------------------------------------
+function compare_ok {
+	local APP=$1
+	test -z "$1" || echo -e "\n$APP: ${#TEST_OUT[@]}\n------------------------------"
 
 	local a=; local i=; local n=; local ok=; local out=; local err=0;
 
@@ -40,10 +96,6 @@ function _compare_ok {
 		fi
 	done
 
-	if test $err -gt 0; then
-		_abort "$APP test failed - fix $err error(s)"
-	else
-		echo -e "------------------------------\n$n test(s) succeded\n"
-	fi
+	test_done $n $err
 }
 
