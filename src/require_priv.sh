85e9bd0dab4a1148cb331cb7b09fbeb00145fc93
Roland Kujundzic <roland@kujundzic.de>
2020-01-07 09:02:10 +0100

    update

--- src/require_priv.sh
+++ src/require_priv.sh
@@ -8,14 +8,9 @@
 # @require _abort
 #--
 function _require_priv {
-	if test -z "$2"; then
-		_abort "empty privileges"
-	fi
-
-	local priv=`stat -c '%a' "$1" || _abort "no such filesystem entry '$1'"`
-
-	if ! test "$2" = "$priv"; then
-		_abort "invalid privileges - chmod $1 '$2'"
-	fi
+	test -z "$2" && _abort "empty privileges"
+	local priv=`stat -c '%a' "$1" 2>/dev/null`
+	test -z "$priv" && _abort "stat -c '%a' '$1'"
+	test "$2" = "$priv" || _abort "invalid privileges [$priv] - chmod -R $2 '$1'"
 }
 
