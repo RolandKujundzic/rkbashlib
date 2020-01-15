3117c5089c2b1eb8c5df69744f666d70ab27d3a0
Roland Kujundzic <roland@inkoeln.com>
2020-01-15 15:27:45 +0100

    allow seed substring

--- src/random_string.sh
+++ src/random_string.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
 #--
-# Print random string of length $1
+# Print random string of length $1 (chars from [0-9a-zA-Z-_]).
+# 
+# @example _random_string n 10 26 = random from [a-z], length n
+# @example _random_string n 36 26 = random from [A-Z], length n
+# @example _random_string n 0 62 = random from [0-9a-zA-Z], length n 
 #
 # @param string length (default = 8)
+# @param string char pos [0-63]
+# @param string char length [1-64]
 #--
 function _random_string {
 	local CHARS="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
 	local LEN=${1:-8}
 	local i
+
+	if ! test -z "$2" && ! test -z "$3"; then
+		CHARS="${CHARS:$2:$3}"
+	fi
 
 	for (( i = 0; i < $1; i++ )); do
 		echo -n "${CHARS:RANDOM%${#CHARS}:1}"
