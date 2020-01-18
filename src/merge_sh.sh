876942788ab39dd8e8d91a84297c7b80186abe22
Roland Kujundzic <roland@kujundzic.de>
2020-01-18 06:14:30 +0100

    update

--- src/merge_sh.sh
+++ src/merge_sh.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 #--
-# Merge $1_ into $1 (concat $1_/*.inc.sh).
+# Merge "$APP"_ into $APP (concat "$APP""_/*.inc.sh").
 #
-# @param string script path
+# @global APP
 # @require _require_file _require_dir _chmod _md5
 #--
 function _merge_sh {
-	_require_file "$1"
-	local SH_DIR=`dirname "$1"`'/_'
+	_require_file "$APP"
+	local SH_DIR="$APP"'_'
 	_require_dir "$SH_DIR"
 
-	local MD5_OLD=`_md5_file "$0"`
-  echo -n "merge $SH_DIR into $1 ... "
+	local MD5_OLD=`_md5_file "$APP"`
+  echo -n "merge $SH_DIR into $APP ... "
 
 	local a
 	for a in "$SH_DIR"/*.inc.sh; do
-		tail -n+2 "$a" >> "$1"
+		tail -n+2 "$a" >> "$APP"
   done
 
-  _chmod 755 "$1"
+  _chmod 755 "$APP"
 
-	local MD5_NEW=`_md5_file "$0"`
+	local MD5_NEW=`_md5_file "$APP"`
 	test "$MD5_OLD"="$MD5_NEW" && echo "no change" || echo "update"
 }
 
