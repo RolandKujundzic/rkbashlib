aa4f718ed7666d42291208163ffc369e4602fc0b
Roland Kujundzic <roland@kujundzic.de>
2020-01-18 15:31:05 +0100

    update

--- src/sql_load.sh
+++ src/sql_load.sh
@@ -2,13 +2,17 @@
 
 #--
 # Load sql dump $1 (ask). Based on rks-db_connect - implement custom _sql_load if missing.
+# If flag=1 load dump without confirmation.
 #
+# @param sql dump
+# @param flag
 # @require _require_program _require_file _confirm
 #--
 function _sql_load {
 	_require_program "rks-db_connect"
 	_require_file "$1"
 
-	_confirm "load sql create dump '$1'?" 1
+	test "$2" = "1" && AUTOCONFIRM=y
+	_confirm "load sql dump '$1'?" 1
 	test "$CONFIRM" = "y" && rks-db_connect load "$1" --q1=n --q2=y
 }
