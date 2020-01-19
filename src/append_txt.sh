b87a146244f002a3b0d7790ec1098afe659c1e90
Roland Kujundzic <roland@inkoeln.com>
2020-01-19 15:47:15 +0100

    create directory if missing

--- src/append_txt.sh
+++ src/append_txt.sh
@@ -5,12 +5,15 @@
 #
 # @param target file
 # @param text
-# @require _abort
+# @require _abort _msg _mkdir
 #--
 function _append_txt {
 	local FOUND=
 	test -f "$1" && FOUND=$(grep "$2" "$1")
 	test -z "$FOUND" || { _msg "$2 was already appended to $1"; return; }
+
+	local DIR=`dirname "$1"`
+	test -d "$DIR" || _mkdir "$DIR"
 
 	_msg "append text '$2' to '$1'"
 	echo "$2" >> "$1" || _abort "echo '$2' >> '$1'"
