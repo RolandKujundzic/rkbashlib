d9c93d1487e182a01d28c42e18bc8c21da18d5ab
Roland Kujundzic <roland@kujundzic.de>
2020-01-20 07:26:36 +0100

    deprecated use _git_update_php instead

--- src/git_update.sh
+++ src/git_update.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 #--
-# Update git components.
-#
-# @require _git_checkout _mkdir
+# @deprecated use _git_update_php
+# @param int flag (2^N, default=3)
+# @require _msg _git_update_php
 #--
 function _git_update {
-	_mkdir php
-	cd php
-	_git_checkout "https://github.com/RolandKujundzic/rkphplib.git" rkphplib
-	_git_checkout "rk@s1.dyn4.com:/data/git/php/phplib.git" phplib
-	cd ..
+	_msg "DEPRECATED: use _git_update_php"
+  _git_update_php $1
 }
 
