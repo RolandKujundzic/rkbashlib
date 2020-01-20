efb8b7d4dd577bdd57a90f231481756bf4c61827
Roland Kujundzic <roland@kujundzic.de>
2020-01-20 07:25:52 +0100

    initial

---
+++ src/git_update_php.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#--
+# Update git components in php/. Flag (default=3):
+#		1: https://github.com/RolandKujundzic/rkphplib.git
+#		2: rk@s1.dyn4.com:/data/git/php/phplib.git
+#
+# @param int flag (2^N, default=3)
+# @require _git_checkout _mkdir _cd
+#--
+function _git_update_php {
+	local FLAG=$1
+	test -z "$FLAG" && FLAG=$(($1 & 0))
+
+	_mkdir php 2>/dev/null
+	_cd php
+
+	test $((FLAG & 1)) -eq 1 && _git_checkout "https://github.com/RolandKujundzic/rkphplib.git" rkphplib
+	test $((FLAG & 2)) -eq 2 && _git_checkout "rk@s1.dyn4.com:/data/git/php/phplib.git" phplib
+
+	_cd ..
+}
+
