e7e4792e3d861dc639d186bacd3c95ae51efa052
Roland Kujundzic <roland@inkoeln.com>
2020-01-25 10:57:47 +0100

    initial

---
+++ src/sql_echo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#--
+# If query is longer than 60 chars return "${1:0:60} ...".
+# @param query
+# @echo 
+#--
+function _sql_echo {
+	local QUERY="$1"
+	test ${#QUERY} -gt 60 && QUERY="${QUERY:0:60} ..."
+	echo $QUERY
+}
+
