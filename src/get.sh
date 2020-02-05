b687082fe48d1fd4338ab6338c85767ac18e73fd
Roland Kujundzic <roland@inkoeln.com>
2020-02-05 10:37:28 +0100

    use static cache dir

--- src/get.sh
+++ src/get.sh
@@ -6,7 +6,9 @@
 # @param string name
 #--
 function _get {
-	local DIR="$RKSCRIPT_DIR/"`basename "$APP"`
+	local DIR="$RKSCRIPT_DIR"
+	test "$DIR" = "$HOME/.rkscript/$$" && DIR="$HOME/.rkscript"
+	DIR="$DIR/"`basename "$APP"`
 
 	test -f "$DIR/$1.nfo" || _abort "no such file $DIR/$1.nfo"
 
--- src/set.sh
+++ src/set.sh
@@ -7,9 +7,11 @@
 # @param string value
 #--
 function _set {
-	local DIR="$RKSCRIPT_DIR/"`basename "$APP"`
+	local DIR="$RKSCRIPT_DIR"
+	test "$DIR" = "$HOME/.rkscript/$$" && DIR="$HOME/.rkscript"
+	DIR="$DIR/"`basename "$APP"`
 
-	test -d "$DIR" || _mkdir "$DIR"
+	test -d "$DIR" || _mkdir "$DIR" >/dev/null
 	test -z "$1" && _abort "empty name"
 
 	echo -e "$2" > "$DIR/$1.nfo"
