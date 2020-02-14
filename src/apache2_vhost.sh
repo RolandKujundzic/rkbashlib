9bf00a29fe1e9f1159408fff1121085d7bb82ea5
Roland Kujundzic <roland@inkoeln.com>
2020-02-14 16:14:47 +0100

    initial

---
+++ src/apache2_vhost.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#--
+# Create vhost link to $2. Create /website/... and docroot.
+# Add $1 domain to /etc/hosts (if *.xx). 
+#
+# @param domain
+# @param docroot
+# @require _mkdir _confirm _cd _ln _run_as_root _append_txt _msg _split
+#--
+function _apache2_vhost {
+	if ! test -d "$2"; then
+		_confirm "Create docroot '$2'?" 1
+		test "$CONFIRM" = "y" && _mkdir "$2"
+	fi
+
+	_split '.' "$1" >/dev/null
+
+	local a
+
+	if test "${#_SPLIT[@]}" -eq 2; then
+		a="/website/${_SPLIT[0]}"'_'"${_SPLIT[1]}"
+		_cd "$a"
+		_ln "$2" '_'
+	else
+		a="/website/${_SPLIT[1]}"'_'"${_SPLIT[2]}"
+		_mkdir "$a"
+		_cd "$a"
+		_ln "$2" "${_SPLIT[0]}"
+	fi
+
+	local IS_XX=`echo "$1" | grep -E '\.xx$'`
+	if ! test -z "$IS_XX"; then
+		_msg "Add $1 domain to /etc/hosts"
+		_run_as_root 1
+		_append_txt /etc/hosts "127.0.0.1 $1"
+	fi
+}
+
