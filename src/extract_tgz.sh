3105f43169352a92fe2790a92ed8566718dbb356
Roland Kujundzic <roland@kujundzic.de>
2020-02-16 11:20:14 +0100

    refactoring

--- src/extract_tgz.sh
+++ src/extract_tgz.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 #--
-# Extract tgz archive $1. If second parameter is existing directory, remove
-# before extraction.
+# Extract tgz archive $1. If second parameter is existing file or directory, 
+# remove before extraction.
 #
 # @param tgz_file
 # @param path (optional - if set check if path was created)
 # @require _abort _rm
 #--
 function _extract_tgz {
+	test -s "$1" || _abort "_extract_tgz: Invalid archive path [$1]"
+	local TARGET="$2"
 
-	if ! test -f "$1"; then
-		_abort "Invalid archive path [$1]"
+	if test -z "$TARGET" && test "${1: -4}" = ".tgz"; then
+		TARGET="${1:0:-4}"
 	fi
 
-	if ! test -z "$2" && test -d $2; then
-		_rm "$2"
+	if ! test -z "$TARGET" && test -d $TARGET; then
+		_rm "$TARGET"
 	fi
+
+	tar -tzf "$1" >/dev/null 2>/dev/null || _abort "_extract_tgz: invalid archive '$1'" 
 
   echo "extract archive $1"
   SECONDS=0
-  tar -xzf $1 || _abort "tar -xzf $1 failed"
+  tar -xzf $1 >/dev/null || _abort "tar -xzf $1 failed"
   echo "$(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds elapsed."
 
-	tar -tzf $1 > /dev/null || _abort "invalid archive $1" 
-
-	if ! test -z "$2"; then
-		if ! test -d "$2" && ! test -f "$2"; then
-			_abort "Path $2 was not created"
-		fi
+	if ! test -z "$TARGET" && ! test -d "$TARGET" && ! test -f "$TARGET"; then
+		_abort "$TAREGET was not created"
 	fi
 }
 
