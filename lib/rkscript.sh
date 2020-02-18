8d642df0c13e6c17d0c47911dff7f06dce91d87e
Roland Kujundzic <roland@kujundzic.de>
2020-02-18 19:21:30 +0100

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -1247,15 +1247,17 @@ function _create_tgz {
 # @param string user
 # @param string repeat-time
 # @param string command
-# @require _abort _msg _mkdir
+# @require _abort _msg _mkdir _require_program
 #--
 function _crontab {
 	local CRONTAB_DIR="/var/spool/cron/crontabs"
-	_msg "install $1 crontab: $2 $3"
+	_msg "install '$1' crontab: [$2 $3] ... " -n
 	_mkdir "$CRONTAB_DIR" >/dev/null
 
-	grep "$3" "$CRONTAB_DIR/$1" 2>/dev/null && { echo "already installed - skip"; return; }
-	echo "$2 $3" >>"$CRONTAB_DIR/$1" || _abort "failed to create $1 cron"
+	_require_program crontab
+	{ crontab -l -u "$1" 2>/dev/null | grep "$3" >/dev/null; } && { _msg "skip (already installed)"; return; }
+	{ ( crontab -l -u root 2>/dev/null; echo "$2 $3" ) | crontab -u "$1" -; } && _msg "ok" || \
+		{ _msg "failed"; _abort "failed to add [$2 $3] to '$1' cron"; }
 }
 
 
