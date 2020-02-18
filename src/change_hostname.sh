cac7bba5af418229f970206782bf0403f7894339
Roland Kujundzic <roland@kujundzic.de>
2020-02-18 14:37:34 +0100

    update

--- src/change_hostname.sh
+++ src/change_hostname.sh
@@ -4,12 +4,13 @@
 # Change hostname if hostname != $1.
 #
 # @param hostname
-# @require _abort _require_program _msg
+# @require _abort _require_program _msg _run_as_root
 #--
 function _change_hostname {
 	local NEW_HNAME="$1"
 	test -z "$NEW_HNAME" && return
 
+	_run_as_root
 	_require_program hostname
 	local CURR_HNAME=`hostname`
 	test "$NEW_HNAME" = "$CURR_HNAME" && return
