6f5627ed4f8ecb9e63e00179ee8ddd5cb4789445
Roland Kujundzic <roland@kujundzic.de>
2020-02-18 18:50:15 +0100

    update

--- src/crontab.sh
+++ src/crontab.sh
@@ -6,11 +6,11 @@
 # @param string user
 # @param string repeat-time
 # @param string command
-# @require _abort
+# @require _abort _msg _mkdir
 #--
 function _crontab {
 	local CRONTAB_DIR="/var/spool/cron/crontabs"
-	log "install $1 crontab: $2 $3"
+	_msg "install $1 crontab: $2 $3"
 	_mkdir "$CRONTAB_DIR" >/dev/null
 
 	grep "$3" "$CRONTAB_DIR/$1" 2>/dev/null && { echo "already installed - skip"; return; }
