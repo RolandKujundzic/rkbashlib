3dcc7f7088af4913087d0ea9d50d8e7d0f03a3ec
Roland Kujundzic <roland@kujundzic.de>
2020-02-18 14:31:46 +0100

    initial

---
+++ src/change_hostname.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#--
+# Change hostname if hostname != $1.
+#
+# @param hostname
+# @require _abort _require_program _msg
+#--
+function _change_hostname {
+	local NEW_HNAME="$1"
+	test -z "$NEW_HNAME" && return
+
+	_require_program hostname
+	local CURR_HNAME=`hostname`
+	test "$NEW_HNAME" = "$CURR_HNAME" && return
+
+	_require_program hostnamectl
+	_msg "change hostname '$CURR_HNAME' to '$NEW_HNAME'"
+	hostnamectl set-hostname "$NEW_HNAME" || _abort "hostnamectl set-hostname '$NEW_HNAME'"
+}
+
