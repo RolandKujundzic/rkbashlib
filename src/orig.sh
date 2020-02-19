17c2fc5876fe9069fd1286c1f6ad0534473712c3
Roland Kujundzic <roland@kujundzic.de>
2020-02-19 15:21:20 +0100

    return bool

--- src/orig.sh
+++ src/orig.sh
@@ -4,26 +4,26 @@
 # Backup $1 as $1.orig (if not already done).
 #
 # @param path
-# @param bool is_optional
-# @require _cp _abort
+# @param bool do not abort if $1 is missing (optional, default = 0 = abort)
+# @require _cp _abort _msg
+# @return bool
 #--
 function _orig {
-	if test -f "$1"; then
-		if test -f "$1.orig"; then
-			echo "Backup $1.orig already exists"
-		else
-			echo "Backup $1 as $1.orig"
-			_cp "$1" "$1.orig"
-		fi
-	elif test -d "$1"; then
-		if test -d "$1.orig"; then
-			echo "Backup $1.orig already exists"
-		else
-			echo "Backup $1 as $1.orig"
-			_cp "$1" "$1.orig"
-		fi
-	elif test -z "$2"; then
-		_abort "missing $1"
+	local RET=0
+
+	if ! test -f "$1" && ! test -d "$1"; then
+		test -z "$2" && _abort "missing $1"
+		RET=1
 	fi
+
+	if test -f "$1.orig"; then
+		_msg "$1.orig already exists"
+		RET=1
+	else
+		_msg "create backup $1.orig"
+		_cp "$1" "$1.orig"
+	fi
+
+	return $RET
 }
 
