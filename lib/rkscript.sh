850754c78e9bf2c53980e23051c346c57dbc3bb6
Roland Kujundzic <roland@inkoeln.com>
2020-03-09 15:22:23 +0100

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -4856,6 +4856,26 @@ function _sudo {
 
 
 #--
+# Dump database on $1:$2. Require rks-db_connect on both server.
+# @param ssh e.g. user@domain.tld
+# @param docroot
+# @require _abort _require_program _msg
+#--
+function _sync_db {
+	_msg "Create database dump in $1:$2/data/.sql with rks-db_connect dump"
+	ssh $1 "cd $2 && rks-db_connect dump >/dev/null" || _abort 
+
+	_msg "Download and import dump"
+	_rsync "$1:$2/data/.sql" "data/" >/dev/null
+
+	local LS_LAST_DUMP="data/.sql/mysql_dump_"`date +'%Y%m%d'`
+	local LAST_DUMP=`ls "$LS_LAST_DUMP"* | tail -1`
+
+	_require_program rks-db_connect
+	rks-db_connect load "$LAST_DUMP" >/dev/null
+}
+
+#--
 # Create php file with includes from source directory.
 #
 # @param source directory
--- src/sync_db.sh
+++ src/sync_db.sh
@@ -4,15 +4,18 @@
 # Dump database on $1:$2. Require rks-db_connect on both server.
 # @param ssh e.g. user@domain.tld
 # @param docroot
-# @require _abort _require_program
+# @require _abort _require_program _msg
 #--
 function _sync_db {
-	ssh $1 "cd $2 && rks-db_connect dump" || _abort 
-	_rsync "$1:$2/data/.sql" "data/"
+	_msg "Create database dump in $1:$2/data/.sql with rks-db_connect dump"
+	ssh $1 "cd $2 && rks-db_connect dump >/dev/null" || _abort 
+
+	_msg "Download and import dump"
+	_rsync "$1:$2/data/.sql" "data/" >/dev/null
 
 	local LS_LAST_DUMP="data/.sql/mysql_dump_"`date +'%Y%m%d'`
 	local LAST_DUMP=`ls "$LS_LAST_DUMP"* | tail -1`
 
 	_require_program rks-db_connect
-	rks-db_connect load "$LAST_DUMP"
+	rks-db_connect load "$LAST_DUMP" >/dev/null
 }
