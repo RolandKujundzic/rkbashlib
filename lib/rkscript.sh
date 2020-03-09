5b682324e00c5ddd7793947b739a4b44894b8c90
Roland Kujundzic <roland@inkoeln.com>
2020-03-09 12:43:56 +0100

    update priv to 644

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -4454,6 +4454,44 @@ function _sql_echo {
 
 
 #--
+# Run sql execute query (no result).
+#
+# @param sql query
+# @param flag (1=execute sql without confirmation)
+# @global _SQL
+# @require _abort _confirm _sql_echo _require_global
+#--
+function _sql_execute {
+	local QUERY="$1"
+	test -z "$QUERY" && _abort "empty sql execute query"
+	_require_global "_SQL"
+	if test "$2" = "1"; then
+		echo "execute sql query: $(_sql_echo "$QUERY")"
+		$_SQL "$QUERY" || _abort "$QUERY"
+	else
+		_confirm "execute sql query: $(_sql_echo "$QUERY")? " 1
+		test "$CONFIRM" = "y" && { $_SQL "$QUERY" || _abort "$QUERY"; }
+	fi
+}
+
+
+#--
+# Print sql select result table.
+#
+# @global _SQL
+# @param type query
+# @require _abort _require_global
+#--
+function _sql_list {
+	local QUERY="$1"
+	test -z "$QUERY" && _abort "empty query in _sql_list"
+	_require_global "_SQL"
+
+	$_SQL "$QUERY" || _abort "$QUERY"
+}
+
+
+#--
 # Load sql dump $1 (ask). Based on rks-db_connect - implement custom _sql_load if missing.
 # If flag=1 load dump without confirmation.
 #
@@ -4470,13 +4508,60 @@ function _sql_load {
 	test "$CONFIRM" = "y" && rks-db_connect load >/dev/null "$1" --q1=n --q2=y
 }
 
+#--
+# Run sql select query. Save result of select query to _SQL_COL. 
+# Add _SQL_COL[_all] (=STDOUT) and _SQL_COL[_rows].
+#
+# BEWARE: don't use `_sql_select ...` or $(_sql_select) - _SQL_COL will be empty (subshell execution)
+#
+# @global _SQL _SQL_PARAM (hash) _SQL_COL (hash)
+# @export SQL (=rks-db_connect query)
+# @param type select|execute
+# @param query or SQL_QUERY key
+# @param flag (1=execute sql without confirmation)
+# @require _abort _confirm _sql_echo _require_global
+# @return boolean (if type=select - false = no result)
+#--
+function _sql_select {
+	local QUERY="$1"
+	test -z "$QUERY" && _abort "empty query in _sql_select"
+	_require_global "_SQL"
+
+	local DBOUT=`$_SQL "$QUERY" || _abort "$QUERY"`
+	local LNUM=`echo "$DBOUT" | wc -l`
+
+	_SQL_COL=()
+	_SQL_COL[_all]="$DBOUT"
+	_SQL_COL[_rows]=$((LNUM - 1))
+
+	if test $LNUM -eq 2; then
+		local LINE1=`echo "$DBOUT" | head -1`
+		local LINE2=`echo "$DBOUT" | tail -1`
+		local CKEY; local CVAL; local i;
+
+		IFS=$'\t' read -ra CKEY <<< "$LINE1"
+		IFS=$'\t' read -ra CVAL <<< "$LINE2"
+
+		for (( i=0; i < ${#CKEY[@]}; i++ )); do
+			_SQL_COL[${CKEY[$i]}]="${CVAL[$i]}"
+		done
+
+		return 0  # true single line result
+	elif test $LNUM -lt 2; then
+		return 1  # false = no result
+	else
+		_abort "_sql select: multi line result ($LNUM lines)\nUse _sql list ..."
+	fi
+}
+
+
 _SQL=
 declare -A _SQL_QUERY
 declare -A _SQL_PARAM
 declare -A _SQL_COL
 
 #--
-# Run sql select or execute query. Query is either $2 or _SQL_QUERY[$2] (if set). 
+# Run _sql[_list|execute|select]. Query is either $2 or _SQL_QUERY[$2] (if set). 
 # If $1=select save result of select query to _SQL_COL. Add _SQL_COL[_all] (=STDOUT) and _SQL_COL[_rows].
 # If $1=execute ask if query $2 should be execute (default=y) or skip. Set _SQL 
 # (default _SQL="rks-db_connect query") and _SQL_QUERY (optional).
@@ -4488,7 +4573,7 @@ declare -A _SQL_COL
 # @param type select|execute
 # @param query or SQL_QUERY key
 # @param flag (1=execute sql without confirmation)
-# @require _abort _confirm _sql_echo
+# @require _abort _confirm _sql_echo _sql_execute
 # @return boolean (if type=select - false = no result)
 #--
 function _sql {
@@ -4510,44 +4595,16 @@ function _sql {
 		done
 	fi
 
-	test -z "$QUERY" && _abort "empty query in _sql $1"
+	test -z "$QUERY" && _abort "empty query in _sql"
 
 	if test "$1" = "select"; then
-		local DBOUT=`$_SQL "$QUERY" || _abort "$QUERY"`
-		local LNUM=`echo "$DBOUT" | wc -l`
-
-		_SQL_COL=()
-		_SQL_COL[_all]="$DBOUT"
-		_SQL_COL[_rows]=$((LNUM - 1))
-
-		if test $LNUM -eq 2; then
-			local LINE1=`echo "$DBOUT" | head -1`
-			local LINE2=`echo "$DBOUT" | tail -1`
-			local CKEY; local CVAL; local i;
-
-			IFS=$'\t' read -ra CKEY <<< "$LINE1"
-			IFS=$'\t' read -ra CVAL <<< "$LINE2"
-
-			for (( i=0; i < ${#CKEY[@]}; i++ )); do
-				_SQL_COL[${CKEY[$i]}]="${CVAL[$i]}"
-			done
-
-			return 0  # true single line result
-		elif test $LNUM -lt 2; then
-			return 1  # false = no result
-		else
-			_abort "ToDo: _sql select multi line result ($LNUM lines)\nQUERY: $QUERY"
-		fi
+		_sql_select "$QUERY"
 	elif test "$1" = "execute"; then
-		if test "$3" = "1"; then
-			echo "execute sql query: $(_sql_echo "$QUERY")"
-			$_SQL "$QUERY" || _abort "$QUERY"
-		else
-			_confirm "execute sql query: $(_sql_echo "$QUERY")? " 1
-			test "$CONFIRM" = "y" && { $_SQL "$QUERY" || _abort "$QUERY"; }
-		fi
+		_sql_execute "$QUERY" $3
+	elif test "$1" = "list"; then
+		_sql_list "$QUERY"
 	else
-		_abort "_sql(...) invalid first parameter [$1] - use select|execute"
+		_abort "_sql(...) invalid first parameter [$1] - use select|execute|list"
 	fi
 }
 
