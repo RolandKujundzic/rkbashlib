31f03c93ce4ea119bfef5222ffe8aaa51d5701c3
Roland Kujundzic <roland@inkoeln.com>
2020-03-10 18:36:43 +0100

    update

--- src/sql.sh
+++ src/sql.sh
@@ -6,10 +6,11 @@ declare -A _SQL_PARAM
 declare -A _SQL_COL
 
 #--
-# Run _sql[_list|execute|select]. Query is either $2 or _SQL_QUERY[$2] (if set). 
+# Run _sql[list|execute|select]. Query is either $2 or _SQL_QUERY[$2] (if set). 
 # If $1=select save result of select query to _SQL_COL. Add _SQL_COL[_all] (=STDOUT) and _SQL_COL[_rows].
 # If $1=execute ask if query $2 should be execute (default=y) or skip. Set _SQL 
 # (default _SQL="rks-db_connect query") and _SQL_QUERY (optional).
+# Use _SEARCH_PARAM[SEARCH] to replace WHERE_SEARCH|AND_SEARCH tag.
 #
 # BEWARE: don't use `_sql select ...` or $(_sql select) - _SQL_COL will be empty (subshell execution)
 #
@@ -30,26 +31,43 @@ function _sql {
 		fi
 	fi
 
+	local ACTION="$1"
 	local QUERY="$2"
-	if ! test -z "${_SQL_QUERY[$2]}"; then
-		QUERY="${_SQL_QUERY[$2]}"
-		local a=
 
-		for a in "${!_SQL_PARAM[@]}"; do
-			QUERY="${QUERY//\'$a\'/\'${_SQL_PARAM[$a]}\'}"
-		done
+	if [[ "$1" =~ ^(list|execute|select)_([a-z]+)$ ]]; then
+		local ACTION="${BASH_REMATCH[1]}"
+		local QUERY="$1"
+		test -z "${_SQL_QUERY[$QUERY]}" && _abort "invalid action $ACTION - no such query key $QUERY"
 	fi
+
+	if ! test -z "${_SQL_QUERY[$QUERY]}"; then
+		QUERY="${_SQL_QUERY[$QUERY]}"
+		local a=
+	fi
+
+	for a in "${!_SQL_PARAM[@]}"; do
+		QUERY="${QUERY//\'$a\'/\'${_SQL_PARAM[$a]}\'}"
+	done
+
+	if ! test -z "${_SQL_PARAM[SEARCH]}"; then
+		QUERY="${QUERY//WHERE_SEARCH/WHERE 1=1 ${_SQL_PARAM[SEARCH]}}"
+		QUERY="${QUERY//AND_SEARCH/${_SQL_PARAM[SEARCH]}}"
+	fi
+
+	for a in WHERE_SEARCH AND_SEARCH; do
+		QUERY="${QUERY//$a/}"
+	done
 
 	test -z "$QUERY" && _abort "empty query in _sql"
 
-	if test "$1" = "select"; then
+	if test "$ACTION" = "select"; then
 		_sql_select "$QUERY"
-	elif test "$1" = "execute"; then
+	elif test "$ACTION" = "execute"; then
 		_sql_execute "$QUERY" $3
-	elif test "$1" = "list"; then
+	elif test "$ACTION" = "list"; then
 		_sql_list "$QUERY"
 	else
-		_abort "_sql(...) invalid first parameter [$1] - use select|execute|list"
+		_abort "_sql(...) invalid first parameter [$1] - use select|execute|list or ACTION_QKEY"
 	fi
 }
 
