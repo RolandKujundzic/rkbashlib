4ae403c442d466520987bd976b6b49d96550feb7
Roland Kujundzic <roland@kujundzic.de>
2020-03-17 09:27:32 +0100

    initial

---
+++ src/port_reachable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#--
+# Check if port $2 on server $1 is reachable
+#
+# @param string ip or server name
+# @param port
+# @return bool
+#--
+function _port_reachable {
+	if nc -zv -w2 $1 $2 2>/dev/null; then
+		return 0
+	else
+		return 1
+	fi
+}
+
