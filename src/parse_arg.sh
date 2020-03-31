ac5629e3bdadac2246cdd806ade75926cf7c5cd8
Roland Kujundzic <roland@inkoeln.com>
2020-03-31 11:40:36 +0200

    add ARGV

--- src/parse_arg.sh
+++ src/parse_arg.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
 declare -A ARG
+declare ARGV
 
 #--
 # Set ARG[name]=value if --name=value or name=value.
-# If --name set ARG[name]=1. Set ARG[0] ... ARG[n].
-# Reset ARG if ARG[0] is set.
+# If --name set ARG[name]=1. Set ARG[0], ARG[1], ... otherwise.
+# (Re)Set ARGV=( $@ ). Don't reset ARG (allow default).
 #
 # @parameter any
 # @global ARG (hash)
 #--
 function _parse_arg {
-	local key=; local i=; local v=;
-	test -z "${ARG[0]}" || ARG=()
+	ARGV=()
 
+	local n=0; local i; local key; local val;
 	for (( i = 0; i <= $#; i++ )); do
-		ARG[$i]="${!i}"
-		v="${!i}"
+		ARGV[$i]="${!i}"
+		val="${!i}"
+		key=
 
-		if [[ $v == "--"*"="* ]]; then
-			key="${v/=*/}"
-			ARG[${key/--/}]="${v/*=/}"
-		elif [[ $v == "--"* ]]; then
-			ARG[${v/--/}]=1
-		elif [[ $v == *"="* ]]; then
-			ARG[${v/=*/}]="${v/*=/}"
+		if [[ $val == "--"*"="* ]]; then
+			key="${val/=*/}"
+			key="${key/--/}"
+			val="${val#*=}"
+		elif [[ $val == "--"* ]]; then
+			key="${val/--/}"
+			val=1
+		elif [[ $val == *"="* ]]; then
+			key="${val/=*/}"
+			val="${val#*=}"
+		fi
+
+		if test -z "$key"; then
+			ARG[$n]="$val"
+			n=$(( n + 1 ))
+		else
+			ARG[$key]="$val"
 		fi
 	done
 }
