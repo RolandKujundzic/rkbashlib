e3ca6a03c72833a5f8390c61dc9670cf164bb7db
Roland Kujundzic <roland@inkoeln.com>
2020-04-17 21:45:37 +0200

    fix missing file bug

--- src/append_txt.sh
+++ src/append_txt.sh
@@ -8,17 +8,19 @@
 # @require _abort _msg _mkdir
 #--
 function _append_txt {
-	local FOUND=
-	test -f "$1" && FOUND=$(grep "$2" "$1")
-	test -z "$FOUND" || { _msg "$2 was already appended to $1"; return; }
+	local found
+	test -f "$1" && found=$(grep "$2" "$1")
+	test -z "$found" || { _msg "$2 was already appended to $1"; return; }
 
-	local DIR=`dirname "$1"`
-	test -d "$DIR" || _mkdir "$DIR"
+	local dir=`dirname "$1"`
+	test -d "$dir" || _mkdir "$dir"
 
 	_msg "append text '$2' to '$1'"
-	if test -w "$1"; then
+	if ! test -f "$1" || test -w "$1"; then
+		echo "append [$2] to [$1]"
 		echo "$2" >> "$1" || _abort "echo '$2' >> '$1'"
 	else
+		echo "sudo append [$2] to [$1]"
 		{ echo "$2" | sudo tee -a "$1" >/dev/null; } || _abort "echo '$2' | sudo tee -a '$1'"
 	fi
 }
