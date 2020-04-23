4bcb2569360d60a147b59fff99edcfa0d93cfe6e
Roland Kujundzic <roland@inkoeln.com>
2020-04-23 11:41:23 +0200

    use SYNTAX_ instead of _SYNTAX

--- src/rks_app.sh
+++ src/rks_app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #--
-# Prepare rks-app. Adjust APP_DESC if _SYNTAX_HELP[$1|$1.$2] is set.
+# Prepare rks-app. Adjust APP_DESC if SYNTAX_HELP[$1|$1.$2] is set.
+# Execute self_update or help action if $1 = self_update|help.
 #
-# global APP_DESC _SYNTAX_CMD _SYNTAX_HELP
-# require _abort _syntax _merge_sh
-# parameter $0 $@
+# @global APP_DESC SYNTAX_CMD SYNTAX_HELP
+# @param $0 $@
 #--
 function _rks_app {
 	local me="$1"
@@ -19,18 +19,18 @@ function _rks_app {
 	fi
 
 	test -z "$APP_DESC" && _abort "APP_DESC is empty"
-	test -z "${#_SYNTAX_CMD[@]}" && _abort "_SYNTAX_CMD is empty"
-	test -z "${#_SYNTAX_HELP[@]}" && _abort "_SYNTAX_HELP is empty"
+	test -z "${#SYNTAX_CMD[@]}" && _abort "SYNTAX_CMD is empty"
+	test -z "${#SYNTAX_HELP[@]}" && _abort "SYNTAX_HELP is empty"
 
 	[[ "$1" =	'self_update' ]] && _merge_sh
 
 	[[ "$1" = "help" ]] && _syntax "*" "cmd:* help:*"
 	test -z "$1" && return
 
-	test -z "${_SYNTAX_HELP[$1]}" || APP_DESC="${_SYNTAX_HELP[$1]}"
-	test -z "${_SYNTAX_HELP[$1.$2]}" || APP_DESC="${_SYNTAX_HELP[$1.$2]}"
+	test -z "${SYNTAX_HELP[$1]}" || APP_DESC="${SYNTAX_HELP[$1]}"
+	test -z "${SYNTAX_HELP[$1.$2]}" || APP_DESC="${SYNTAX_HELP[$1.$2]}"
 
-	[[ ! -z "${_SYNTAX_CMD[$1]}" && "$2" = 'help' ]] && _syntax "$1" "help:"
-	[[ ! -z "${_SYNTAX_CMD[$1.$2]}" && "$3" = 'help' ]] && _syntax "$1.$2" "help:"
+	[[ ! -z "${SYNTAX_CMD[$1]}" && "$2" = 'help' ]] && _syntax "$1" "help:"
+	[[ ! -z "${SYNTAX_CMD[$1.$2]}" && "$3" = 'help' ]] && _syntax "$1.$2" "help:"
 }
 
