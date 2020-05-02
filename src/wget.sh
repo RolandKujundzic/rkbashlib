b01046e37369a9642ff98d42c45410885aae9575
Roland Kujundzic <roland@inkoeln.com>
2020-05-03 00:16:54 +0200

    shellcheck _wget

--- src/wget.sh
+++ src/wget.sh
@@ -7,14 +7,16 @@
 # @param save as default = autodect, use "-" for stdout
 #--
 function _wget {
+	local save_as
+
 	test -z "$1" && _abort "empty url"
 	_require_program wget
 
-	local SAVE_AS=${2:-`basename "$1"`}
-	if test -s "$SAVE_AS"; then
-		_confirm "Overwrite $SAVE_AS" 1
+	save_as=${2:-$(basename "$1")}
+	if test -s "$save_as"; then
+		_confirm "Overwrite $save_as" 1
 		if test "$CONFIRM" != "y"; then
-			echo "keep $SAVE_AS - skip wget '$1'"
+			echo "keep $save_as - skip wget '$1'"
 			return
 		fi
 	fi
@@ -30,10 +32,11 @@ function _wget {
 		wget -q -O "$2" "$1" || _abort "wget -q -O '$2' '$1'"
 	fi
 
+	local new_files
 	if test -z "$2"; then
-		if ! test -s "$SAVE_AS"; then
-			local NEW_FILES=`find . -amin 1 -type f`
-			test -z "$NEW_FILES" && _abort "Download from $1 failed"
+		if ! test -s "$save_as"; then
+			new_files=$(find . -amin 1 -type f)
+			test -z "$new_files" && _abort "Download from $1 failed"
 		fi
 	elif ! test -s "$2"; then
 		_abort "Download of $2 from $1 failed"
