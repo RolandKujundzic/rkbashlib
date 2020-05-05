060ba42e80b099845e2b1510f6a678c3a5d577b6
Roland Kujundzic <roland@inkoeln.com>
2020-05-05 14:49:43 +0200

    refactoring

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -562,21 +562,20 @@ function _cd {
 # Abort if ssl certificate is missing or does not contain subdomain.
 #
 # @param string domain
+# @export CERT_DNS
 # @param string subdomain list (optional)
+# shellcheck disable=SC2034
 #--
 function _cert_domain {
 	_cert_file "$1"
-	local has_domain
 
-	has_domain=$(openssl x509 -text -noout -in "$CERT_FULL" | grep "DNS:*.$1")
-	test -z "$has_domain" || return
+	CERT_DNS=$(openssl x509 -text -noout -in "$CERT_FULL" | grep "DNS:" | _trim)
 
-	has_domain=$(openssl x509 -text -noout -in "$CERT_FULL" | grep "DNS:$1")
-	test -z "$has_domain" && _abort "missing domain $1 in $CERT_FULL"
+	[[ "$CERT_DNS" =~ DNS:*.$1 ]] && return
+	[[ "$CERT_DNS" =~ DNS:$1 ]] || _abort "missing domain $1 in $CERT_FULL"
 
 	for a in $2; do
-		has_domain=$(openssl x509 -text -noout -in "$CERT_FULL" | grep "DNS:$a.$1")
-		test -z "$has_domain" && _abort "missing domain $a.$1 in $CERT_FULL"
+		[[ "$CERT_DNS" =~ DNS:$a.$1 ]] || _abort "missing domain $a.$1 in $CERT_FULL"
 	done
 }
 
@@ -5343,10 +5342,12 @@ function _toupper {
 #--
 # Print trimmed string. 
 #
-# @param string name
+# @param string name (use /dev/stdin if not set)
 #--
 function _trim {
-	echo -e "$1" | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//'
+	local input
+	test -z "${1+x}" && input=$(cat /dev/stdin) || input="$1"
+	echo -e "$input" | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//'
 }
 
 #--
