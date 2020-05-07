b95014079378fe1dae040579ecca3cd24268da7e
Roland Kujundzic <roland@inkoeln.com>
2020-05-07 17:52:22 +0200

    update

--- src/cert_domain.sh
+++ src/cert_domain.sh
@@ -3,27 +3,39 @@
 #--
 # Abort if ssl certificate is missing or does not contain subdomain.
 #
-# @param string domain
-# @export CERT_DNS CERT_GMT CERT_DOMAINS
+# @param string domain|path/to/fullchain.pem
+# @export CERT_DNS CERT_GMT CERT_DOMAINS CERT_UNTIL CERT_FULL
 # @param string subdomain list (optional)
 # shellcheck disable=SC2034
 #--
 function _cert_domain {
-	_cert_file "$1"
+	local domain
+
+	if test -f "$1"; then
+		CERT_FULL="$1"
+	else
+		_cert_file "$1"
+		domain="$1"
+	fi
 
 	local certinfo dns
 	certinfo=$(openssl x509 -text -noout -in "$CERT_FULL")
 	dns=$(openssl x509 -in "$CERT_FULL" -text | grep "DNS:" | sed -E -e 's/,? ?DNS\:/ /g' | _trim)
 
+	if test -z "$domain"; then
+		domain=$(echo "$certinfo" | grep -E -o 'CN = .+' | grep -v 'Encrypt Authority' | sed 's/CN = //')
+	fi
+
 	CERT_GMT=$(echo "$certinfo" | grep "GMT" | _trim)
 	CERT_DNS=$(echo "$certinfo" | grep "DNS:" | _trim)
-	CERT_DOMAINS=( $dns )
+	CERT_UNTIL=$(echo "$certinfo" | grep "GMT" | grep -o -E 'Not After .+' | sed -E -e 's/.+\: (.+ GMT).*/\1/i') 
+	CERT_DOMAINS=( "$dns" )
 
-	[[ "$CERT_DNS" =~ DNS:*.$1 ]] && return
-	[[ "$CERT_DNS" =~ DNS:$1 ]] || _abort "missing domain $1 in $CERT_FULL"
+	[[ "$CERT_DNS" =~ DNS:*.$domain ]] && return
+	[[ "$CERT_DNS" =~ DNS:$domain ]] || _abort "missing domain $domain in $CERT_FULL"
 
 	for a in $2; do
-		[[ "$CERT_DNS" =~ DNS:$a.$1 ]] || _abort "missing domain $a.$1 in $CERT_FULL"
+		[[ "$CERT_DNS" =~ DNS:$a.$domain ]] || _abort "missing domain $a.$domain in $CERT_FULL"
 	done
 }
 
