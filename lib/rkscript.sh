4aa0e7539b8d98aafe1e88a2c967d356f7c7fb5e
Roland Kujundzic <roland@inkoeln.com>
2020-05-10 13:43:06 +0200

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -2603,14 +2603,15 @@ function _license {
 # @param link path
 #--
 function _ln {
+	local target target_dir link_dir old_target
 	_require_program realpath
 
-	local target=`realpath "$1"`
+	target=$(realpath "$1")
 	test -z "$target" && _abort "no such directory [$1]"
 	test "$2" = "$target" && _abort "ln -s '$target' '$2' # source=target"
 
 	if test -L "$2"; then
-		local old_target=`realpath "$2"`
+		old_target=$(realpath "$2")
 
 		if test "$target" = "$old_target"; then
 			echo "Link $2 to $target already exists"
@@ -2620,15 +2621,16 @@ function _ln {
 		_rm "$2"
 	fi
 
-	local link_dir=`dirname "$2"`
-	link_dir=`realpath "$link_dir"`
-	local target_dir=`dirname "$target"`
+	link_dir=$(dirname "$2")
+	link_dir=$(realpath "$link_dir")
+	target_dir=$(dirname "$target")
 
+	local tname lname cwd
 	if test "$target_dir" = "$link_dir"; then
-		local cwd="$PWD"
+		cwd="$PWD"
 		_cd "$target_dir"
-		local tname=`basename "$1"`
-		local lname=`basename "$2"`
+		tname=$(basename "$1")
+		lname=$(basename "$2")
 		echo "ln -s '$tname' '$lname' # in $PWD"
 		ln -s "$tname" "$lname" || _abort "ln -s '$tname' '$lname' # in $PWD"
 		_cd "$cwd"
