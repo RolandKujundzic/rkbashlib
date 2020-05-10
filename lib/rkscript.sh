35add71b7adc72a20ed278c7961c0f01995c57d4
Roland Kujundzic <roland@inkoeln.com>
2020-05-10 20:37:45 +0200

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -18,7 +18,7 @@ else
 	exit 1  
 fi  
 
-if test "${@: -1}" = 'help'; then
+if test "${@: -1}" = 'help' 2>/dev/null; then
 	for a in ps tr xargs head grep awk find sed sudo cd chown chmod mkdir rm ls; do
 		command -v $a >/dev/null || { echo -e "\nERROR: missing $a\n"; exit 1; }
 	done
@@ -584,6 +584,7 @@ function _cert_domain {
 	else
 		_cert_file "$1"
 		domain="$1"
+		test -z "$CERT_SUB" || domain="$CERT_SUB"
 	fi
 
 	local certinfo dns
@@ -612,6 +613,7 @@ function _cert_domain {
 # Export path to SSL certificate files:
 #
 # - CERT_ENGINE=acme.sh|certbot
+# - CERT_SUB=sub.domain.tld
 # - CERT_FULL=~/.acme.sh/domain.tld/fullchain.cer or /etc/letsencrypt/live/domain.tld/fullchain.pem
 # - CERT_KEY=~/.acme.sh/domain.tld/domain.tld.key or /etc/letsencrypt/live/domain.tld/privkey.pem
 # - CERT_PUB=~/.acme.sh/domain.tld/domain.tld.cer or /etc/letsencrypt/live/domain.tld/cert.pem
@@ -620,60 +622,74 @@ function _cert_domain {
 # @param domain.tld
 # @param abort if missing (default = 1)
 # @export CERT_ENGINE|FULL|KEY|CA|PUB
-# shellcheck disable=SC2034
+# shellcheck disable=SC2034,SC2086
 # return boolean
 #--
 function _cert_file {
-	local domain res
+	local domain res acme_dir le_live le_acme subdomain
+	acme_dir="$HOME/.acme.sh/$1"
+	le_live="/etc/letsencrypt/live/$1"
+	le_acme="/etc/letsencrypt/acme.sh/$1"
 	domain="$1"
 
 	test -z "$domain" && _abort "empty domain parameter"
 
 	CERT_ENGINE=
+	CERT_SUB=
 	CERT_FULL=
 	CERT_KEY=
 	CERT_PUB=
 	CERT_CA=
 	res=1
 
-	if test -s "$HOME/.acme.sh/$domain/fullchain.cer"; then
+	if test -s "$acme_dir/fullchain.cer"; then
 		CERT_ENGINE="acme.sh"
-		CERT_FULL="$HOME/.acme.sh/$domain/fullchain.cer"
-		CERT_KEY="$HOME/.acme.sh/$domain/$domain.key"
-		CERT_PUB="$HOME/.acme.sh/$domain/$domain.cer"
-		CERT_CA="$HOME/.acme.sh/$domain/ca.cer"
+	else
+		subdomain=$(ls $HOME/.acme.sh/*.$domain/fullchain.cer)
+		if ! test -z "$subdomain" && test -s "$subdomain"; then
+			acme_dir=$(dirname $subdomain)
+			CERT_ENGINE="acme.sh"
+			CERT_SUB=$(basename $acme_dir)
+		fi
+	fi
+
+	if test "$CERT_ENGINE" = "acme.sh"; then
+		CERT_FULL="$acme_dir/fullchain.cer"
+		CERT_KEY="$acme_dir/$subdomain.key"
+		CERT_PUB="$acme_dir/$subdomain.cer"
+		CERT_CA="$acme_dir/ca.cer"
 		res=0
 	fi
 
 	if test "$UID" = "0" && ! test -z "$CERT_FULL"; then
-		if test -L "/etc/letsencrypt/live/$domain" || test -L "/etc/letsencrypt/live/$domain/fullchain.pem"; then
-			CERT_FULL="/etc/letsencrypt/live/$domain/fullchain.pem"
-			CERT_KEY="/etc/letsencrypt/live/$domain/privkey.pem"
-			CERT_PUB="/etc/letsencrypt/live/$domain/cert.pem"
-			CERT_CA="/etc/letsencrypt/live/$domain/chain.pem"
-		elif test -s "/etc/letsencrypt/acme.sh/$domain/fullchain.pem"; then
-			CERT_FULL="/etc/letsencrypt/acme.sh/$domain/fullchain.pem"
-			CERT_KEY="/etc/letsencrypt/acme.sh/$domain/privkey.pem"
-			CERT_PUB="/etc/letsencrypt/acme.sh/$domain/cert.pem"
-			CERT_CA="/etc/letsencrypt/acme.sh/$domain/chain.pem"
+		if test -L "$le_live" || test -L "$le_live/fullchain.pem"; then
+			CERT_FULL="$le_live/fullchain.pem"
+			CERT_KEY="$le_live/privkey.pem"
+			CERT_PUB="$le_live/cert.pem"
+			CERT_CA="$le_live/chain.pem"
+		elif test -s "$le_acme/fullchain.pem"; then
+			CERT_FULL="$le_acme/fullchain.pem"
+			CERT_KEY="$le_acme/privkey.pem"
+			CERT_PUB="$le_acme/cert.pem"
+			CERT_CA="$le_acme/chain.pem"
 		fi
 	fi
 
 	if test -z "$CERT_FULL"; then
-		test "$UID" = "0" || echo "missing $HOME/.acme.sh/$domain/fullchain.cer - change into root to read /etc/letsencrypt/..."
+		test "$UID" = "0" || echo "missing $acme_dir/fullchain.cer - change into root to read /etc/letsencrypt/..."
 		_run_as_root
-		if test -d "/etc/letsencrypt/archive/$domain" && test -L "/etc/letsencrypt/live/$domain/fullchain.pem"; then
+		if test -d "/etc/letsencrypt/archive/$domain" && test -L "$le_live/fullchain.pem"; then
 			CERT_ENGINE="certbot"
-			CERT_FULL="/etc/letsencrypt/live/$domain/fullchain.pem"
-			CERT_KEY="/etc/letsencrypt/live/$domain/privkey.pem"
-			CERT_PUB="/etc/letsencrypt/live/$domain/cert.pem"
-			CERT_CA="/etc/letsencrypt/live/$domain/chain.pem"
+			CERT_FULL="$le_live/fullchain.pem"
+			CERT_KEY="$le_live/privkey.pem"
+			CERT_PUB="$le_live/cert.pem"
+			CERT_CA="$le_live/chain.pem"
 			res=0
 		fi
 	fi
 
 	[[ -z "$CERT_FULL" && "$2" != "0" ]] && \
-		_abort "found neither $HOME/.acme.sh/$domain/fullchain.cer nor /etc/letsencrypt/live/$domain/fullchain.pem"
+		_abort "found neither $acme_dir/fullchain.cer nor $le_live/fullchain.pem"
 
 	return $res
 }
@@ -2289,28 +2305,30 @@ function _install_node {
 # Export ip address as IP_ADDRESS (ip4) and IP6_ADDRESS (ip6) (and DYNAMIC_IP).
 #
 # @export IP_ADDRESS IP6_ADDRESS DYNAMIC_IP
+# shellcheck disable=SC2034
 #--
 function _ip_address {
+	local ip6_dyn host ping_ok
 	_require_program ip
 
-	IP_ADDRESS=`ip route get 1 | grep -E ' src [0-9\.]+ uid ' | sed -e 's/.* src //' | sed -e 's/ uid.*//'`
+	IP_ADDRESS=$(ip route get 1 | grep -E ' src [0-9\.]+ uid ' | sed -e 's/.* src //' | sed -e 's/ uid.*//')
 	if test -z "$IP_ADDRESS"; then
-		IP_ADDRESS=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+		IP_ADDRESS=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 	fi
 
-	IP6_ADDRESS=`ip -6 addr | grep 'scope global' | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d'`
-	local ip6_dyn=`ip -6 addr | grep 'scope global temporary dynamic' | awk '{print $2}' | sed -e 's/\/[0-9]*$//'`
+	IP6_ADDRESS=$(ip -6 addr | grep 'scope global' | sed -e's/^.*inet6 \([^ ]*\)\/.*$/\1/;t;d')
+	ip6_dyn=$(ip -6 addr | grep 'scope global temporary dynamic' | awk '{print $2}' | sed -e 's/\/[0-9]*$//')
 	if ! test -z "$ip6_dyn"; then
 		IP6_ADDRESS="$ip6_dyn"
 		DYNAMIC_IP=1
 	fi
 
 	_require_program ping
-	local host=`hostname`
-	local ping_ok=`ping -4 -c 1 "$host" 2>/dev/null | grep "$IP_ADDRESS"`
+	host=$(hostname)
+	ping_ok=$(ping -4 -c 1 "$host" 2>/dev/null | grep "$IP_ADDRESS")
 
 	if test -z "$ping_ok"; then
-		ping_ok=`ping -4 -c 1 "$host" 2>/dev/null | grep "127.0."`
+		ping_ok=$(ping -4 -c 1 "$host" 2>/dev/null | grep "127.0.")
 
 		if test -z "$ping_ok"; then
 			_abort "failed to detect IP_ADDRESS (ping -4 -c 1 $host != $IP_ADDRESS)"
