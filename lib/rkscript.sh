e6294e67ddc0c90eab3dc894531bbfbb1b784ecb
Roland Kujundzic <roland@inkoeln.com>
2020-05-13 19:26:28 +0200

    update

--- lib/rkscript.sh
+++ lib/rkscript.sh
@@ -3362,9 +3362,8 @@ function _mysql_restore {
 # @param php_file (if empty search for docroot with settings.php and|or index.php)
 # @param int don't abort (default = 0 = abort)
 # @global DOCROOT PATH_RKPHPLIB
-# @export DB_NAME DB_PASS MYSQL DOCROOT
+# @export DB_NAME (DB_LOGIN) DB_PASS MYSQL DOCROOT
 # @return bool
-# shellcheck disable=SC2016
 #--
 function _mysql_split_dsn {
 	local rkphplib settings_dsn php_code split_dsn
@@ -3378,60 +3377,48 @@ function _mysql_split_dsn {
 		test -z "$DOCROOT" && { _find_docroot "$PWD" "$2" || return 1; }
 
 		if test -f "$DOCROOT/settings.php"; then
-			_mysql_split_dsn "$DOCROOT/settings.php" "$2" && return 0 || return 1
+			_settings_php "$DOCROOT/settings.php"
 		elif test -f "$DOCROOT/index.php"; then
-			_mysql_split_dsn "$DOCROOT/index.php" "$2" && return 0 || return 1
+			_settings_php "$DOCROOT/settings.php"
 		fi
-
-		test -z "$2" && _abort "no such file [$1]" || return 1
+	else
+		_settings_php "$1"
 	fi
 
-	php_code='ob_start(); include("'$1'"); $html = ob_get_clean(); if (defined("settings_dsn")) print settings_dsn;'
-	settings_dsn=$(php -r "$php_code")
+	[[ -z "$DB_NAME" || -z "$DB_PASS" ]] || return 0
 
-	if test -z "$rkphplib"; then
-		php_code='ob_start(); include("'$1'"); $html = ob_get_clean(); if (defined("rkphplib")) print rkphplib;'
-		rkphplib=$(php -r "$php_code")
-	fi
-	
-	if test -z "$rkphplib" && test -d "/webhome/.php/rkphplib/src"; then
-		rkphplib="/webhome/.php/rkphplib/src/"
-	fi
+	test -z "$2" && _abort "autodetect DB_NAME|PASS failed"
+	return 1
+}
 
-	if test -z "$settings_dsn" && test -f "settings.php"; then
-		php_code='ob_start(); include("settings.php"); $html = ob_get_clean(); if (defined("settings_dsn")) print settings_dsn;'
-		settings_dsn=$(php -r "$php_code")
-		DOCROOT="$PWD"
-	fi
 
-	if ! test -z "$DOCROOT" && ! test -z "$rkphplib" && ! test -f "$rkphplib/Exception.class.php" && \
-			test -f "$DOCROOT/$rkphplib/Exception.class.php"; then
-		rkphplib="$DOCROOT/$rkphplib"
-	fi
+#--
+# Load settings.php via php and export SETTINGS_(DB_NAME|DB_PASS|DSN), PATH_(RKPHPLIB|PHPLIB) and DOCROOT.
+# @param settings.php path
+# shellcheck disable=SC2016
+#--
+function _settings_php {
+	local php_code php_out
 
-	if test -z "$settings_dsn"; then
-		test -z "$2" && _abort "autodetect settings_dsn failed" || return 1
-	fi
- 
-	if test -z "$rkphplib"; then
-		if test -d "$HOME/workspace/rkphplib/src"; then
-			rkphplib="$HOME/workspace/rkphplib/src/"
-		else
-			test -z "$2" && _abort "autodetect rkphplib failed - export rkphplib=/path/to/rkphplib/src/" || return 1
-		fi
-	fi
+	IFS='' read -r -d '' php_code <<'EOF'
+ob_start();
+include(getenv('SETTINGS_PHP'));
 
-	local split_dsn='require("'$rkphplib'ADatabase.class.php"); $dsn = \rkphplib\ADatabase::splitDSN("'$settings_dsn'");'
+if (defined('SETTINGS_DB_NAME') && defined('SETTINGS_DB_PASS') && !empty(SETTINGS_DB_NAME) && !empty(SETTINGS_DB_PASS)) {
+	print "DB_NAME='".SETTINGS_DB_NAME."'\nDB_PASS='".SETTINGS_DB_PASS."'";
+}
+else if (defined('SETTINGS_DSN') && defined('PATH_RKPHPLIB')) {
+	require(const('PATH_RKPHPLIB').'ADatabase.class.php');
+	$dsn = \rkphplib\ADatabase::splitDSN(SETTINGS_DSN);
+	print "DB_NAME='".$dsn['login']."'\nDB_PASS='".$dsn['password']."'";
+	if ($dsn['login'] != $dsn['name']) {
+		print "\nDB_LOGIN='".$dsn['login']."'";
+	}
+}
+EOF
 
-	php_code=$split_dsn' print $dsn["login"];'
-	DB_NAME=$(php -r "$php_code")
-
-	php_code=$split_dsn' print $dsn["password"];'
-	DB_PASS=$(php -r "$php_code")
-
-	if test -z "$DB_NAME" || test -z "$DB_PASS"; then
-		test -z "$2" && _abort "database name [$DB_NAME] or password [$DB_PASS] is empty" || return 1
-	fi
+	php_out=$(php -r "$php_code")
+	test -z "$php_out" || echo "$php_out"
 }
 
 
