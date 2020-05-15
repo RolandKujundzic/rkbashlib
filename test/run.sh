a931ed2e8e805f5bf6e62be023d7dc22d0b690ec
Roland Kujundzic <roland@inkoeln.com>
2020-05-15 23:57:44 +0200

    _shell_check added

--- test/run.sh
+++ test/run.sh
@@ -5,32 +5,12 @@
 
 
 #--
-# Run shellcheck for *.sh files in directory and subdirectories
-# shellcheck disable=SC2044
-#--
-function runShellCheck {
-	_require_dir "$1"
-	local sh_files file
-	sh_files=( $(find "$1" -name '*.sh' 2>/dev/null) )
-
-	PROGRESS_MAX=${#sh_files[@]}
-	PROGRESS_LABEL="shellcheck"
-	for ((PROGRESS_VALUE=0; PROGRESS_VALUE < ${#sh_files[@]}; PROGRESS_VALUE++)); do
-		file="${sh_files[$PROGRESS_VALUE]}"
-		PROGRESS_MSG="$file"
-		_progress_bar "$PROGRESS_VALUE"
-		shellcheck "$file" &>/dev/null || _abort "shellcheck $file"
-	done
-}
-
-
-#--
 # M A I N
 #--
 
 source ../lib/rkscript.sh || { echo "failed to load ../lib/rkscript.sh"; exit 1; }
 
-runShellCheck ../src
+_shell_check ../src
 
 source testHelper.sh || { echo "failed to load testHelper.sh"; exit 1; }
 
