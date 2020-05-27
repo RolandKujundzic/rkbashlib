5c44c65154ee3691e8b92785e67172c7c1907d75
Roland Kujundzic <roland@inkoeln.com>
2020-05-27 17:33:24 +0200

    _gzip added

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -2125,6 +2125,19 @@ function _gunzip {
 }
 
 
+#--
+# Gzip $1
+# @param file path
+#--
+function _gzip {
+	_require_file "$1"
+	_msg "gzip $1"
+	_require_program gzip
+	gzip "$1" || _abort "gzip '$1'"
+	_require_file "$1.gz"
+}
+
+
 declare -A PROCESS
 
 #--
