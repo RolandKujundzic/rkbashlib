4b204476d1dfc699cf07f6e91426b9210ed12f68
Roland Kujundzic <roland@inkoeln.com>
2020-05-28 07:51:55 +0200

    moved to shell/doc

--- example/group_commands.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-crontab -l -u root 2>/dev/null; echo "* * * * * /path/to/new/cron" | less
-( crontab -l -u root 2>/dev/null; echo "* * * * * /path/to/new/cron"; ) | less
-{ crontab -l -u root 2>/dev/null; echo "* * * * * /path/to/new/cron"; } | less
--- example/random.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# select random number from [1 ... 100]
-RND=`shuf -i 1-100 -n 1`
-F=0`bc -l <<< "scale=2; $RND/100"`
-echo "RND=$RND F=$F"
