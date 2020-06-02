af46ae6d290d1c0543f53ad8e65fee3809e2ef69
Roland Kujundzic <roland@inkoeln.com>
2020-06-02 12:07:33 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -1932,6 +1932,20 @@ function _find {
 
 
 #--
+# Apply (perl) regular expression replace s/$2/$3/g on file $1
+# @param regular expression
+# @param file
+#--
+function _frx_replace {
+	[[ -z "$2" ]] && _abort "invalid regular expression s/$2/$3/g"
+	_require_program perl
+	_require_file "$1"
+
+	perl -i -pe "s/$2/$3/g" "$1"
+}
+
+
+#--
 # Return saved value ($RKBASH_DIR/$APP/name.nfo).
 #
 # @param string name
