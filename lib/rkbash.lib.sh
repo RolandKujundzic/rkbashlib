262d68a26a36768ec12a3cd2bfa13b01f5d70d80
Roland Kujundzic <roland@inkoeln.com>
2020-06-30 10:08:17 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -4648,9 +4648,9 @@ function _scp {
 	local user host path md5 md5remote
 
 	if [[ "$1" =~ ^(.+)@(.+):(/.+) ]]; then
-		md5=$(_md5 "$2")
+		test -f "$2" && md5=$(_md5 "$2")
 	elif [[ "$2" =~ ^(.+)@(.+):(/.+) ]]; then
-		md5=$(_md5 "$1")
+		test -f "$1" && md5=$(_md5 "$1")
 	else
 		_abort "neither [$1] or [$2] are remote"
 	fi
@@ -4659,13 +4659,17 @@ function _scp {
 	host="${BASH_REMATCH[2]}"
 	path="${BASH_REMATCH[3]}"
 
-	md5remote=$(ssh "$user@$host" "md5sum '$path'" 2>/dev/null | awk '{print $1}')
-	
+	if test -z "$md5"; then
+		md5remote=1
+	else
+		md5remote=$(ssh "$user@$host" "md5sum '$path'" 2>/dev/null | awk '{print $1}')
+	fi
+
 	if test "$md5" = "$md5remote"; then
 		echo "$path at $host has not changed" 
 	else
 		echo "scp $1 $2"
-		scp "$1" "$2" >/dev/null || _abort "_scp $1 $2" 
+		scp "$1" "$2" >/dev/null || _abort "scp $1 $2" 
 	fi
 }
 
