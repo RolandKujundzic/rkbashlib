0bd2f79bf4685a032e655517a437d53d7c4790ff
Roland Kujundzic <roland@inkoeln.com>
2020-07-05 11:16:28 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -4509,15 +4509,24 @@ function _rkbash {
 # Prepare rks-app. Adjust APP_DESC if SYNTAX_HELP[$1|$1.$2] is set.
 # Execute self_update or help action if $1 = self_update|help.
 #
+# @example _parse_arg "$@"; APP_DESC='...'; _rks_app "$0" "$@"
 # @global APP_DESC SYNTAX_CMD SYNTAX_HELP
 # @export APP CURR APP_DIR APP_PID (if not set)
 # @param $0 $@
 # shellcheck disable=SC2034,SC2119
 #--
 function _rks_app {
-	local me="$1"
-	test -z "$me" && _abort "call _rks_app '$0' $*"
+	local me p1 p2 p3
+	me="$1"
 	shift
+	p1="$1"
+	p2="$2"
+	p3="$3"
+
+	test -z "$me" && _abort "call _rks_app '$0' $*"
+	test -z "${ARG[1]}" || p1="${ARG[1]}"
+	test -z "${ARG[2]}" || p2="${ARG[2]}"
+	test -z "${ARG[3]}" || p3="${ARG[3]}"
 
 	if test -z "$APP"; then
 		APP="$me"
@@ -4534,16 +4543,16 @@ function _rks_app {
 	test -z "${#SYNTAX_CMD[@]}" && _abort "SYNTAX_CMD is empty"
 	test -z "${#SYNTAX_HELP[@]}" && _abort "SYNTAX_HELP is empty"
 
-	[[ "$1" =	'self_update' ]] && _merge_sh
+	[[ "$p1" =	'self_update' ]] && _merge_sh
 
-	[[ "$1" = 'help' ]] && _syntax "*" "cmd:* help:*"
-	test -z "$1" && return
+	[[ "$p1" = 'help' ]] && _syntax "*" "cmd:* help:*"
+	test -z "$p1" && return
 
-	test -z "${SYNTAX_HELP[$1]}" || APP_DESC="${SYNTAX_HELP[$1]}"
-	test -z "${SYNTAX_HELP[$1.$2]}" || APP_DESC="${SYNTAX_HELP[$1.$2]}"
+	test -z "${SYNTAX_HELP[$p1]}" || APP_DESC="${SYNTAX_HELP[$p1]}"
+	test -z "${SYNTAX_HELP[$p1.$p2]}" || APP_DESC="${SYNTAX_HELP[$p1.$p2]}"
 
-	[[ ! -z "${SYNTAX_CMD[$1]}" && "$2" = 'help' ]] && _syntax "$1" "help:"
-	[[ ! -z "${SYNTAX_CMD[$1.$2]}" && "$3" = 'help' ]] && _syntax "$1.$2" "help:"
+	[[ ! -z "${SYNTAX_CMD[$p1]}" && "$p2" = 'help' ]] && _syntax "$p1" "help:"
+	[[ ! -z "${SYNTAX_CMD[$p1.$p2]}" && "$p3" = 'help' ]] && _syntax "$p1.$p2" "help:"
 }
 
 
