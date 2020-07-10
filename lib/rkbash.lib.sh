5420cde0170c0dcf0ddac4e56c78310ec61dbc18
Roland Kujundzic <roland@inkoeln.com>
2020-07-10 11:47:01 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -4473,6 +4473,22 @@ function _require_program {
 
 
 #--
+# Abort if '$1 --version' is lowner than $2.
+#
+# @param app name
+# @param app version
+#--
+function _require_version {
+	_require_program "$1"
+	local version
+	version="$($1 --version 2>/dev/null | sed -E 's/.+ version ([0-9]+\.[0-9]+)\.?([0-9]*).+/\1\2/')"
+	if (( $(echo "$version < $2" | bc -l) )); then
+		_abort "$1 --version < $2"
+	fi
+}
+
+
+#--
 # Export required $RKBASH_SRC/src/* functions as $RKBASH_INC
 #
 # @global RKBASH_SRC (default = .)
