7ba1aa662ef69cdc6e4ef8bfd8900694fe482e45
Roland Kujundzic <roland@inkoeln.com>
2020-07-20 11:37:20 +0200

    refactoring

--- src/cdn_dl.sh
+++ src/cdn_dl.sh
@@ -1,47 +1,26 @@
 #!/bin/bash
 
 #--
-# Download source url to target path.
+# Download source url to target path. 
+# Export CDN_HTML to save <script ... and <link ... to ./CDN_HTML 
 #
-# @global DOCROOT if not empty and head.inc.html exists copy files here and append to head.inc.html
-# @global CDN_DIR path prefix (if empty use ./)
-#
+# @global CDN_HTML
 # @param string source url
 # @param string target path
 # shellcheck disable=SC2046
 #--
 function _cdn_dl {
-	local suffix target has_file
-	suffix=$(echo "$2" | awk -F . '{print $NF}')
-	target="$2"
+	_wget "$1" "$2"
 
-	if test -z "$CDN_DIR"; then
-		target="./$target"
-	else
-		target="$CDN_DIR/$target"
-	fi
-
-	_mkdir $(dirname "$target")
-	_download "$1" "$target"
-	_download "$1.map" "$target.map" 1
-
-	if test -n "$DOCROOT"; then
-		_cp "$target" "$DOCROOT/$2"
-
-		if test -f "$target.map"; then
-			_cp "$target.map" "$DOCROOT/$2.map"
-		fi
-
-		if test -f "$DOCROOT/head.inc.html"; then
-			has_file=$(grep "=\"$2\"" "$DOCROOT/head.inc.html")
-
-			if test -n "$has_file"; then
-				echo "$2 is already in head.inc.html"
-			elif test "$suffix" = "css" && test -f "$DOCROOT/$2"; then
-				sed -e "s/<\/head>/<link rel=\"stylesheet\" href=\"$2\" \/>/g" > "$DOCROOT/head.inc.html"
-			elif test "$suffix" = "js" && test -f "$DOCROOT/$2"; then
-				sed -e "s/<\/head>/<script src=\"$2\"><\/script>/g" > "$DOCROOT/head.inc.html"
-			fi
+	if test -n "$CDN_HTML"; then
+		if grep -q "\"$2\"" -- *.html; then
+			:
+		elif [[ ! "$CDN_HTML" =~ \.html$ ]]; then
+			_abort "invalid CDN_HTML=$CDN_HTML"
+		elif [[ "$2" =~ \.css$ ]]; then
+			echo "<link rel=\"stylesheet\" href=\"$2\" />" >> "$CDN_HTML"
+		elif [[ "$2" =~ \.js$ ]]; then
+			echo "<script src=\"$2\"><\/script>" >> "$CDN_HTML"
 		fi
 	fi
 }
--- src/download.sh
+++ src/download.sh
@@ -8,18 +8,17 @@
 # @param bool allow_fail
 #--
 function _download {
-	test -z "$2" && _abort "Download target path is empty"
 	test -z "$1" && _abort "Download url is empty"
-	test -z "$3" && echo "Download $1 as $2"
-
+	test -z "$2" && _abort "Download target path is empty"
+	test -z "$3" && _msg "Download $1 as $2"
 
 	_mkdir "$(dirname "$2")"
 	_wget "$1" "$2"
-	[[ -z "$3" && ! -s "$2" ]] && _abort "Download of $2 as $1 failed"
+	[[ -z "$3" && ! -s "$2" ]] && _abort "Download of $1 to $2 failed"
 
 	if test -n "$3"; then
 		if test -s "$2"; then
-			echo "Download $1 as $2"
+			_msg "Download $1 as $2"
 		elif test -f "$2"; then
 			_rm "$2"
 		fi
