6a2562125de52cf1b1c9191fcfa1ed21129b4665
Roland Kujundzic <roland@inkoeln.com>
2020-07-31 16:09:37 +0200

    update

--- src/service.sh
+++ src/service.sh
@@ -2,13 +2,20 @@
 
 #--
 # Control service start|stop|restart|reload|enable|disable
+# @param service name
+# @param action
 #--
-function service {
+function _service {
 	test -z "$1" && _abort "empty service name"
+	test -z "$2" && _abort "empty action"
+
 	local has_service
 	has_service=$(service --status-all | grep -E '\s+'"$1"'$')
 	test -z "$has_service" && _abort "no such service $1"
 
 	[[ ! "$has_service" =~ + && "$1" != 'enable' ]] && _abort "$1 is disabled"
+
+	_msg "service $1 $2"
+	_sudo "service $1 $2"
 }
 
