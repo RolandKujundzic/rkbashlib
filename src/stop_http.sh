b4b75af07fae58fc3f42ce0df168e227d6031839
Roland Kujundzic <roland@inkoeln.com>
2020-07-31 16:07:04 +0200

    use _service

--- src/stop_http.sh
+++ src/stop_http.sh
@@ -10,25 +10,19 @@ function _stop_http {
 	_os_type linux
 
 	if ! _is_running port:80; then
-		echo "no service on port 80"
+		_warn "no service on port 80"
 		return
 	fi
 
 	if _is_running docker:80; then
-		echo "ignore docker service on port 80"
+		_warn "ignore docker service on port 80"
 		return
 	fi
 
 	if _is_running nginx; then
-		echo "stop nginx"
-		sudo service nginx stop
-		return
-	fi
-
-	if _is_running apache; then
-		echo "stop apache2"
-		sudo service apache2 stop
-		return
+		_service nginx stop
+	elif _is_running apache; then
+		_service apache2 stop
 	fi
 }
 
