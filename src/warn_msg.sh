e37b8775603692cd3fdf8465b2beafa13b4b1fa9
Roland Kujundzic <roland@inkoeln.com>
2020-08-09 13:19:07 +0200

    fix first line

--- src/warn_msg.sh
+++ src/warn_msg.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 #--
-# @example x="Error\nInfo" && echo -e "$(_warn_msg $x)"
+# @example x="Error\nInfo" && echo -e "$(_warn_msg "$x")"
 # @param multiline
 # @return multiline with first line in red
 #--
 function _warn_msg {
-	local msg="$1"
-	shift
-	echo '\033[0;31m'"$msg"'\033[0m'
-	echo "$*"
+	local line first
+	while IFS= read -r line; do
+		if test "$first" = '1'; then
+			echo "$line"
+		else
+			echo '\033[0;31m'"$line"'\033[0m'
+			first=1
+		fi
+	done <<< "$@"
 }
 
