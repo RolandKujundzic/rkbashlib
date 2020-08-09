8abc86dff92a5f169a6d6ed309cdf1001455a0a3
Roland Kujundzic <roland@inkoeln.com>
2020-08-09 13:19:17 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -5638,7 +5638,6 @@ declare -A SYNTAX_HELP
 # @global SYNTAX_CMD SYNTAX_HELP APP APP_DESC APP_DESC_2 APP_DESC_3 APP_DESC_4 $APP_PREFIX 
 # @param message
 # @param info (e.g. cmd:* = show all SYNTAX_CMD otherwise show cmd|help:[name] = SYNTAX_CMD|SYNTAX_HELP[name])
-# shellcheck disable=SC2086
 #--
 function _syntax {
 	local a msg old_msg desc base syntax
@@ -5663,9 +5662,9 @@ function _syntax {
 
 	base=$(basename "$APP")
 	if test -n "$APP_PREFIX"; then
-		echo -e "$syntax $(_warn_msg $APP_PREFIX $base $msg)" 1>&2
+		echo -e "$syntax $(_warn_msg "$APP_PREFIX $base $msg")" 1>&2
 	else
-		echo -e "$syntax $(_warn_msg $base $msg)" 1>&2
+		echo -e "$syntax $(_warn_msg "$base $msg")" 1>&2
 	fi
 
 	for a in APP_DESC APP_DESC_2 APP_DESC_3 APP_DESC_4; do
@@ -5888,15 +5887,20 @@ function _version {
 
 
 #--
-# @example x="Error\nInfo" && echo -e "$(_warn_msg $x)"
+# @example x="Error\nInfo" && echo -e "$(_warn_msg "$x")"
 # @param multiline
 # @return multiline with first line in red
 #--
 function _warn_msg {
-	local msg="$1"
-	shift
-	echo '\033[0;31m'"$msg"'\033[0m'
-	echo "$*"
+	local line first
+	while IFS= read -r line; do
+		if test "$first" = '1'; then
+			echo "$line"
+		else
+			echo '\033[0;31m'"$line"'\033[0m'
+			first=1
+		fi
+	done <<< "$@"
 }
 
 
