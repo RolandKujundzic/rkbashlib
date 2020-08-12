fec2718114e5834491f60f35826fc0245d4d6f9f
Roland Kujundzic <roland@inkoeln.com>
2020-08-12 13:08:47 +0200

    use eval "find ..."

--- src/find.sh
+++ src/find.sh
@@ -3,20 +3,27 @@
 #--
 # Save found filesystem entries into FOUND.
 #
+# @example:
+# _find root/dir '-name \*.html'
+# for ((i=0; i < ${#FOUND[@]}; i++)); do
+#   echo "$i: [${FOUND[$i]}]"
+# done
+# @:
+#
 # @param root directory 
-# @param find expression e.g. -name *.html
+# @param find expression e.g. "-name '*.html'"
 # @export FOUND Path Array
 # shellcheck disable=SC2086
 #--
 function _find {
 	FOUND=()
-	local a=
+	local a
 
 	_require_program find
 	_require_dir "$1"
 
 	while read -r a; do
 		FOUND+=("$a")
-	done <<< "$(find "$1" $2 2>/dev/null)"
+	done < <(eval "find '$1' $2" || _abort "find '$1' $2")
 }
 
