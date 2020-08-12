fc4773dbf0633dac5a62bb429dc3facf40a61c3d
Roland Kujundzic <roland@inkoeln.com>
2020-08-12 13:08:53 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -1965,21 +1965,28 @@ function _find_docroot {
 #--
 # Save found filesystem entries into FOUND.
 #
+# @example:
+# _find root/dir '-name \*.html'
+# for ((i=0; i < ${#FOUND[@]}; i++)); do
+#   echo "$i: [${FOUND[$i]}]"
+# done
+# @:
+#
 # @param root directory 
-# @param find expression e.g. -name *.html
+# @param find expression e.g. "-name '*.html'"
 # @export FOUND Path Array
 # shellcheck disable=SC2086
 #--
 function _find {
 	FOUND=()
-	local a=
+	local a
 
 	_require_program find
 	_require_dir "$1"
 
 	while read -r a; do
 		FOUND+=("$a")
-	done <<< "$(find "$1" $2 2>/dev/null)"
+	done < <(eval "find '$1' $2" || _abort "find '$1' $2")
 }
 
 
