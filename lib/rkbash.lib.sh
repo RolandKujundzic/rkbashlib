8992e3bbf367af54b9999ea1f2d7d4d1dad40e34
Roland Kujundzic <roland@inkoeln.com>
2020-08-23 12:16:22 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -4635,6 +4635,28 @@ function _require_version {
 
 
 #--
+# @example _resolv domain2ip some.domain.tld
+# @example _resolv ip2domain 8.8.8.8
+#--
+function _resolv {
+	local res
+
+	if test "$1" = 'domain2ip'; then
+		test -z "$2" _syntax '_resolve domain2ip some.domain.tld'
+		res=$(host "$2" | grep 'dyn4.de has address' | sed -E 's/.+has address //')
+	elif test "$1" = 'ip2domain'; then
+		test -z "$2" _syntax '_resolve ip2domain 8.8.8.8'
+		res=$(nslookup "$2" | grep 'name = ' | sed -E 's/.+name = (.+)\./\1/')
+	else
+		_syntax '_resolv domain2ip|ip2domain'
+	fi
+
+	test -z "$res" && _abort "_resolv '$1' '$2' failed"
+	echo -n "$res"
+}
+
+
+#--
 # Change RKBASH_DIR to ~/.rkbash/$1 if directory is default. 
 # Use $1 = reset to change to ~/.rkbash/$$.
 # 
