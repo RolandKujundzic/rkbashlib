0d1cb7ef03dfb6284993921e37ae5abd3727bf80
Roland Kujundzic <roland@inkoeln.com>
2020-08-25 12:56:24 +0200

    fix bug, add required

--- src/link_target.sh
+++ src/link_target.sh
@@ -5,6 +5,7 @@
 # If found return realpath.
 #
 # @param link
+# @param optional 1=abort if missing
 # @export LINK_TARGET_MISSING
 # shellcheck disable=SC2034
 #--
@@ -12,13 +13,17 @@ function _link_target {
 	local target
 	test -z "$1" && _abort "empty link path"
 
-	target=$(realpath "$1" 2>/dev/null)
-	
-	if test -z "$target"; then
-		target=$(file "$1" | grep -E -o 'symbolic link to .*/webhome/.+' | sed -E 's/symbolic link to //')
+	if [[ -f "$1" || -d "$1" ]]; then
+		target=$(realpath "$1" 2>/dev/null)
+	fi
+
+	if [[ -z "$target" && -L "$1" ]]; then
+		target=$(file "$1" | grep -E 'symbolic link to ' | sed -E 's/.*symbolic link to //')
 		test -z "$target" && _abort "link target detection failed\n$1"
 		LINK_TARGET_MISSING=1
 	fi
+
+	[[ -z "$target" && "$2" = 1 ]] && _abort "missing link $1"
 
 	echo "$target"
 }
