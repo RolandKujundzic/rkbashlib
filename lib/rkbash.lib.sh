5fec056459449610f09417e03e320cca1b982b43
Roland Kujundzic <roland@inkoeln.com>
2020-08-25 08:26:26 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -471,11 +471,14 @@ function _aws {
 #--
 # Backup (realpath) $1 as RKBASH_DIR/backup/$1
 # Keep last n backups. Do not backup if last
-# backup is younger than 5sec.
+# backup is younger than 5sec or was done within
+# this script.
 #
 # @global RKBASH_DIR
+# @export BACKUP_FILE
 # @param path
 # @param keep (default = 5)
+# shellcheck disable=SC2034
 #--
 function _backup_file {
 	local i n path dir base backup backup_dir
@@ -488,7 +491,12 @@ function _backup_file {
 	backup="$backup_dir/$base"
 	n="${2:-5}"
 
-	[[ -f "$backup" && $(stat -c %Y "$backup") -ge $(( $(date +%s) - 5 )) ]] && return
+	test "$backup" = "$BACKUP_FILE" && return
+
+	if [[ -f "$backup" && $(stat -c %Y "$backup") -ge $(( $(date +%s) - 5 )) ]]; then
+		_msg "keep existing backup (younger than 5s)"
+		return
+	fi
 
 	_msg "backup $path"
 	_mkdir "$backup_dir"
@@ -496,6 +504,7 @@ function _backup_file {
 	test -f "$backup" && _cp "$backup" "$backup.old" >/dev/null
 
 	_cp "$path" "$backup" md5
+	BACKUP_FILE="$backup"
 
 	if [[ "$CP_FIRST" = '1' || "$CP_KEEP" = '1' ]]; then
 		test -f "$backup.old" && _rm "$backup.old" >/dev/null
