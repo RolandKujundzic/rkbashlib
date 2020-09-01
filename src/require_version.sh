bfba3247807725472bd8dce7515375546f96b480
Roland Kujundzic <roland@inkoeln.com>
2020-09-01 12:24:43 +0200

    allow return bool

--- src/require_version.sh
+++ src/require_version.sh
@@ -2,16 +2,23 @@
 
 #--
 # Abort if '$1 --version' is lowner than $2.
+# Use NO_ABORT=1 to return 1 if version is wrong.
 #
 # @param app name
 # @param app version
+# @return bool
 #--
 function _require_version {
 	_require_program "$1"
+
 	local version
 	version="$($1 --version 2>/dev/null | sed -E 's/.+ version ([0-9]+\.[0-9]+)\.?([0-9]*).+/\1\2/')"
+
 	if (( $(echo "$version < $2" | bc -l) )); then
 		_abort "$1 --version < $2"
+		return 1
 	fi
+
+	return 0
 }
 
