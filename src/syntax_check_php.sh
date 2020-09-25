9bc5127520ccf2e4821ea26861d047e30678b49a
Roland Kujundzic <roland@inkoeln.com>
2020-09-25 08:38:43 +0200

    test/run.sh added

--- src/syntax_check_php.sh
+++ src/syntax_check_php.sh
@@ -2,6 +2,7 @@
 
 #--
 # Create php file with includes from source directory.
+# If source directory is test execute test/run.sh
 #
 # @param source directory
 # @param output file
@@ -11,6 +12,18 @@
 #--
 function _syntax_check_php {
 	local a php_files php_bin
+
+	if test "$1" = 'test'; then
+		_require_file 'test/run.sh'
+		_cd test
+		_msg "Running test/run.sh ... " -n
+		if ! ./run.sh >/dev/null; then
+			_abort 'test failed - see test/run.log'
+		fi
+		_msg "OK"
+		return
+	fi
+
 	php_files=$(find "$1" -type f -name '*.php')
 	php_bin=$(grep -R -E '^#\!/usr/bin/php' "bin" | grep -v 'php -c skip_syntax_check' | sed -E 's/\:\#\!.+//')
 
