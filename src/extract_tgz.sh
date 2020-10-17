968e8cb2cccafad21a97e623da9c29feb2f9095d
Roland Kujundzic <roland@inkoeln.com>
2020-10-17 11:38:21 +0200

    update

--- src/extract_tgz.sh
+++ src/extract_tgz.sh
@@ -23,10 +23,10 @@ function _extract_tgz {
 
 	tar -tzf "$1" >/dev/null 2>/dev/null || _abort "_extract_tgz: invalid archive '$1'"
 
-  echo "extract archive $1"
-  SECONDS=0
-  tar -xzf "$1" >/dev/null || _abort "tar -xzf $1 failed"
-  echo "$((SECONDS / 60)) minutes and $((SECONDS % 60)) seconds elapsed."
+	_msg "extract archive $1"
+	SECONDS=0
+	tar -xzf "$1" >/dev/null || _abort "tar -xzf $1 failed"
+	_msg "$((SECONDS / 60)) minutes and $((SECONDS % 60)) seconds elapsed."
 
 	if [[ -n "$target" && ! -d "$target" && ! -f "$target" ]]; then
 		_abort "$target was not created"
