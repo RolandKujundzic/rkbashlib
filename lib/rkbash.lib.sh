f82881dd1e11fda2004408a00648c568433c8da3
Roland Kujundzic <roland@inkoeln.com>
2020-10-20 13:08:32 +0200

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -1655,6 +1655,28 @@ function _crontab {
 
 
 #--
+# Create tar.gz archive. Flags:
+# 1 = 2^0: --absolute-names
+# 2 = 2^1: --to-stdout
+#
+# @param flag (use 0 for default -czf)
+# @param archive
+# @param * (other options, sources)
+# shellcheck disable=SC2086,SC2048
+#--
+function _ctar {
+	local flag opt
+	flag=$(($1 + 0))
+	shift
+	opt='-czf'
+
+	test $((flag & 1)) = 1 && opt="--absolute-names $opt" 
+	test $((flag & 2)) = 2 && opt="$opt --to-stdout" 
+
+	tar $opt $* || _abort "tar $opt $*"
+}
+
+#--
 # Decrypt $1. Second parameter is either empty (=ask password), password or password-file (basename starts with dot).
 # If decrypted file is *.tgz archive extract it.
 #
