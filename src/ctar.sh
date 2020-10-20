b16d7f58060e6804cf156cbccb2f15bd228c8199
Roland Kujundzic <roland@inkoeln.com>
2020-10-20 13:08:24 +0200

    initial

---
+++ src/ctar.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#--
+# Create tar.gz archive. Flags:
+# 1 = 2^0: --absolute-names
+# 2 = 2^1: --to-stdout
+#
+# @param flag (use 0 for default -czf)
+# @param archive
+# @param * (other options, sources)
+# shellcheck disable=SC2086,SC2048
+#--
+function _ctar {
+	local flag opt
+	flag=$(($1 + 0))
+	shift
+	opt='-czf'
+
+	test $((flag & 1)) = 1 && opt="--absolute-names $opt" 
+	test $((flag & 2)) = 2 && opt="$opt --to-stdout" 
+
+	tar $opt $* || _abort "tar $opt $*"
+}
