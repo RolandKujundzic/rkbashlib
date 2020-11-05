5f20fb001edda795a09b9684a3c5832a55fa1454
Roland Kujundzic <roland@inkoeln.com>
2020-11-05 13:31:53 +0100

    update

--- lib/rkbash.lib.sh
+++ lib/rkbash.lib.sh
@@ -816,21 +816,36 @@ function _change_fullname {
 #--
 # Change hostname if hostname != $1.
 #
-# @param hostname
+# @param name.domain.tld
 #--
 function _change_hostname {
-	local new_hname curr_hname
-	new_hname="$1"
-	test -z "$new_hname" && return
+	local hname ip hs hd
+
+	[[ -z "$1" || ! "$1" =~ ^[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-]+$ ]]&& \
+		_abort "invalid hostname '$1' use name.domain.tld"
 
 	_run_as_root
-	_require_program hostname
-	curr_hname=$(hostname)
-	test "$new_hname" = "$curr_hname" && return
 
+	hname=$(hostname)
+
+	_require_program hostname
 	_require_program hostnamectl
-	_msg "change hostname '$curr_hname' to '$new_hname'"
-	hostnamectl set-hostname "$new_hname" || _abort "hostnamectl set-hostname '$new_hname'"
+
+	if [[ "$1" != "$hname" ]]; then
+		_msg "change hostname '$hname' to '$1'"
+		hostnamectl set-hostname "$1" || _abort "hostnamectl set-hostname '$1'"
+	fi
+
+	ip=$(hostname -i)
+	hs=$(hostname -s)
+	hd=$(hostname -d)
+
+	_require_file /etc/hosts
+
+	if test -z "$(grep "$ip $hs.$hd $hs" /etc/hosts)"; then
+		_msg "append '$ip $hs.$hd $hs' to /etc/hosts"
+		echo "$ip $hs.$hd $hs" >> /etc/hosts
+	fi
 }
 
 
