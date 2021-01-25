62a0cccef2b704d10bf160fe10b74a548e7e470e
Roland Kujundzic <roland@inkoeln.com>
2021-01-25 10:09:04 +0100

    version update

--- src/node_current.sh
+++ src/node_current.sh
@@ -3,14 +3,14 @@
 #--
 # Check node.js version. Install node and npm if missing. 
 # Update to NODE_VERSION and NPM_VERSION if necessary.
-# Use NODE_VERSION=v15.5.0 and NPM_VERSION=7.3 as default.
+# Use NODE_VERSION=v15.6.0 and NPM_VERSION=7.4.0 as default.
 #
 # @global NODE_VERSION NPM_VERSION
 # @export NODE_VERSION NPM_VERSION
 #--
 function _node_current {
-	test -z "$NODE_VERSION" && NODE_VERSION=v15.5.0
-	test -z "$NPM_VERSION" && NPM_VERSION=7.3
+	test -z "$NODE_VERSION" && NODE_VERSION=v15.6.0
+	test -z "$NPM_VERSION" && NPM_VERSION=7.4.0
 
 	if ! command -v node >/dev/null || ! command -v npm >/dev/null; then
 		_install_node 
