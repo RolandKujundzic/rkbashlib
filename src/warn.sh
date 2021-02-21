788618d0ab5d3b5074dfa66c8b92642cbdeacd18
Roland Kujundzic <roland@inkoeln.com>
2021-02-21 10:53:27 +0100

    update

--- src/warn.sh
+++ src/warn.sh
@@ -4,8 +4,13 @@
 # Print warning (red color message to stdout and stderr)
 #
 # @param message
+# @param optional -n
 #--
 function _warn {
-	echo -e "\033[0;31m$1\033[0m" 1>&2
+	if test "$2" == '-n'; then
+		echo -n -e "\033[0;31m$1\033[0m" 1>&2
+	else
+		echo -e "\033[0;31m$1\033[0m" 1>&2
+	fi
 }
 
